d063abb8d9a51de2945203824df86f097ef0a41f
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-09 14:49:35 -0500

    Add event ordering

--- opendg/events.py
+++ opendg/events.py
@@ -1,14 +1,22 @@
+import functools
 from abc import ABC
-from typing import Optional, Tuple
+from typing import Any, Optional, Tuple
 
 from torch import Tensor
 
 
+@functools.total_ordering
 class Event(ABC):
     r"""An abstract event that occured in a dynamic graph."""
 
     def __init__(self, time: int) -> None:
         self._time = time
+
+    def __lt__(self, other: Any) -> bool:
+        if not isinstance(other, Event):
+            raise ValueError(f'Cannot compare Event type with type: {type(other)}')
+
+        return self.time < other.time
 
     @property
     def time(self) -> int:
--- test/test_events.py
+++ test/test_events.py
@@ -22,6 +22,28 @@ def test_node_event_with_node_feat():
     assert torch.equal(event.features, node_features)
 
 
+def test_node_event_ordering_same_node_id():
+    e1 = NodeEvent(time=1337, node_id=0)
+    e2 = NodeEvent(time=1338, node_id=0)
+    assert e1 < e2
+    assert e1 <= e2
+    assert e1 != e2
+    assert e1 == e1
+    assert e2 >= e1
+    assert e2 > e1
+
+
+def test_node_event_ordering_different_node_id():
+    e1 = NodeEvent(time=1337, node_id=0)
+    e2 = NodeEvent(time=1338, node_id=1)
+    assert e1 < e2
+    assert e1 <= e2
+    assert e1 != e2
+    assert e1 == e1
+    assert e2 >= e1
+    assert e2 > e1
+
+
 def test_edge_event_without_node_feat():
     time = 1337
     edge = (0, 1)
@@ -39,3 +61,36 @@ def test_edge_event_with_node_feat():
     assert event.time == time
     assert event.edge == edge
     assert torch.equal(event.features, edge_features)
+
+
+def test_edge_event_ordering_same_edge():
+    e1 = EdgeEvent(time=1337, edge=(0, 1))
+    e2 = EdgeEvent(time=1338, edge=(0, 1))
+    assert e1 < e2
+    assert e1 <= e2
+    assert e1 != e2
+    assert e1 == e1
+    assert e2 >= e1
+    assert e2 > e1
+
+
+def test_edge_event_ordering_different_node_id():
+    e1 = EdgeEvent(time=1337, edge=(0, 1))
+    e2 = EdgeEvent(time=1338, edge=(1, 2))
+    assert e1 < e2
+    assert e1 <= e2
+    assert e1 != e2
+    assert e1 == e1
+    assert e2 >= e1
+    assert e2 > e1
+
+
+def test_event_ordering_between_events():
+    e1 = NodeEvent(time=1337, node_id=0)
+    e2 = EdgeEvent(time=1338, edge=(1, 2))
+    assert e1 < e2
+    assert e1 <= e2
+    assert e1 != e2
+    assert e1 == e1
+    assert e2 >= e1
+    assert e2 > e1
