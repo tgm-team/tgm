d5add3ee3c00f4796685b1f58b645f70d26c8691
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2025-01-09 13:06:02 -0500

    first version of timedelta

--- opendg/typing.py
+++ opendg/typing.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Tuple
-
+from _storage.timedelta import TimeDeltaTG
 Event = Tuple[int, int, int]  # Timestamp, Source Vertex, Target Vertex
 
 EventsDict = Dict[int, List[Tuple[int, int]]]  # Timestamp -> List of edges
 
-TimeDelta = int  # TODO: Implement a proper object
+TimeDeltaTG = TimeDeltaTG  # TODO: Implement a proper object
