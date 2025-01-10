d7ca8a2949bef45e571f6e2668190b0d2826c232
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-10 17:37:54 -0500

    Add __repr__ to events

--- opendg/events.py
+++ opendg/events.py
@@ -38,6 +38,9 @@ class NodeEvent(Event):
         self._node_id = node_id
         self._features = features
 
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(time={self.time}, node_id={self.node_id}, features={self.features})'
+
     @property
     def node_id(self) -> int:
         return self._node_id
@@ -60,6 +63,9 @@ class EdgeEvent(Event):
 
         self._edge = edge
         self._features = features
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(time={self.time}, edge={self.edge}, features={self.features})'
 
     @property
     def edge(self) -> Tuple[int, int]:
