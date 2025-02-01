a368bf0817298a42960f4cf926b2a1dee40be2ff
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-02-01 13:52:18 -0500

    Run git hooks

--- test/test_timedelta.py
+++ test/test_timedelta.py
@@ -1,23 +1,18 @@
-import pytest
-
-
 from opendg.timedelta import TimeDeltaTG
 
 
 def test_timedelta():
-
-    td1 = TimeDeltaTG('Y',1)
-    td2 = TimeDeltaTG('M',1)
+    td1 = TimeDeltaTG('Y', 1)
+    td2 = TimeDeltaTG('M', 1)
 
     assert td1.unit == 'Y'
     assert td1.value == 1
 
     rate1 = td1.convert('M')
-    assert rate1 == (365/30)
+    assert rate1 == (365 / 30)
     rate2 = td1.convert(td2)
     assert rate2 == rate1
 
-    td3 = TimeDeltaTG('M',3)
+    td3 = TimeDeltaTG('M', 3)
     rate = td1.convert(td3)
-    assert rate == ((365/30)/3)
-
+    assert rate == ((365 / 30) / 3)
