970c90a368b9348477508eefbada007940180a46
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-02-01 14:26:22 -0500

    Remove dead code

--- opendg/typing.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, List, Tuple
-
-Event = Tuple[int, int, int]  # Timestamp, Source Vertex, Target Vertex
-
-EventsDict = Dict[int, List[Tuple[int, int]]]  # Timestamp -> List of edges
