f0ed5d1f223bf3cb2260d5bddfd6ca085ef17ec0
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-03 13:54:40 -0500

    Consolidate csv cols

--- opendg/_io/csv.py
+++ opendg/_io/csv.py
@@ -8,25 +8,26 @@ from opendg.events import EdgeEvent, Event, NodeEvent
 
 def read_csv(
     file_path: str,
-    src_node_id_col: str = 'src_id',
-    dst_node_id_col: str = 'dst_id',
-    time_col: str = 'timestamp',
-    feature_cols: Optional[List[str]] = None,
+    src_col: str,
+    dst_col: str,
+    time_col: Optional[str] = None,
+    edge_feature_col: Optional[List[str]] = None,
 ) -> List[Event]:
     # TODO: Node Feature not supported
     events: List[Event] = []
     with open(file_path, newline='') as f:
         reader = csv.DictReader(f)
-        for row in reader:
-            src_id = int(row[src_node_id_col])
-            dst_id = int(row[dst_node_id_col])
-            time = int(row[time_col])
+        for i, row in enumerate(reader):
+            src_id = int(row[src_col])
+            dst_id = int(row[dst_col])
 
-            if feature_cols is None:
+            time = int(row[time_col]) if time_col is not None else i
+
+            if edge_feature_col is None:
                 features = None  # TODO:: Infer the feature columns
             else:
                 features_list = [
-                    float(row[feature_col]) for feature_col in feature_cols
+                    float(row[feature_col]) for feature_col in edge_feature_col
                 ]
                 features = torch.tensor(features_list)
 
@@ -38,15 +39,15 @@ def read_csv(
 def write_csv(
     events: List[Event],
     file_path: str,
-    src_node_id_col: str = 'src_id',
-    dst_node_id_col: str = 'dst_id',
-    time_col: str = 'timestamp',
-    feature_cols: Optional[List[str]] = None,
+    src_col: str,
+    dst_col: str,
+    time_col: str,
+    edge_feature_col: Optional[List[str]] = None,
 ) -> None:
     with open(file_path, 'w', newline='') as f:
-        fieldnames = [src_node_id_col, dst_node_id_col, time_col]
-        if feature_cols is not None:
-            fieldnames += feature_cols
+        fieldnames = [src_col, dst_col, time_col]
+        if edge_feature_col is not None:
+            fieldnames += edge_feature_col
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -60,13 +61,13 @@ def write_csv(
             features = event.features
 
             row: Dict[str, Any] = {
-                src_node_id_col: src_id,
-                dst_node_id_col: dst_id,
+                src_col: src_id,
+                dst_col: dst_id,
                 time_col: time,
             }
 
             if features is not None:
-                if feature_cols is None:
+                if edge_feature_col is None:
                     raise ValueError(
                         'No feature column provided but events had features'
                     )
@@ -74,14 +75,14 @@ def write_csv(
                 if len(features.shape) > 1:
                     raise ValueError('Multi-dimensional features not supported')
 
-                if len(features) != len(feature_cols):
+                if len(features) != len(edge_feature_col):
                     raise ValueError(
                         f'Got {len(features)}-dimensional feature tensor but only '
-                        f'specified {len(feature_cols)} feature column names.'
+                        f'specified {len(edge_feature_col)} feature column names.'
                     )
 
                 features_list = features.tolist()
-                for feature_col, feature_val in zip(feature_cols, features_list):
+                for feature_col, feature_val in zip(edge_feature_col, features_list):
                     row[feature_col] = feature_val
 
             writer.writerow(row)
