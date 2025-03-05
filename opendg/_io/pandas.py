4b81737f88dde5ee99e2a3592f0a63012e0ae636
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-05 10:32:57 -0500

    Lazy check pandas version

--- opendg/_io/pandas.py
+++ opendg/_io/pandas.py
@@ -14,6 +14,7 @@ def read_pandas(
     time_col: str,
     edge_feature_col: Optional[str] = None,
 ) -> List[Event]:
+    _check_pandas_import()
     # TODO: Node Events not supported
 
     # Pre-allocating buffer of events with the right size, and adding an index column
@@ -40,3 +41,23 @@ def read_pandas(
     df.drop('index', axis=1)  # Clean up temporary index column
 
     return events
+
+
+def _check_pandas_import(min_version_number: Optional[str] = None) -> None:
+    try:
+        import pandas
+
+        user_pandas_version = pandas.__version__
+    except ImportError:
+        user_pandas_version = None
+
+    err_msg = 'User requires pandas '
+    if min_version_number is not None:
+        err_msg += f'>={min_version_number} '
+    err_msg += 'to initialize a DGraph using DGraph.from_pandas()'
+
+    if user_pandas_version is None:
+        raise ImportError(err_msg)
+    elif min_version_number is not None and user_pandas_version < min_version_number:
+        err_msg += f', found pandas=={user_pandas_version} < {min_version_number}'
+        raise ImportError(err_msg)
