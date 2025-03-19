1c5abcf23a08c87c74ded6c26e340e246d6cd66d
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-18 21:28:49 -0400

    Code refactors

--- opendg/graph.py
+++ opendg/graph.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 import pathlib
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 from torch import Tensor
@@ -19,25 +21,20 @@ class DGraph:
         time_delta: TimeDeltaDG = TimeDeltaDG(unit='r'),
         **kwargs: Any,
     ) -> None:
+        if not isinstance(time_delta, TimeDeltaDG):
+            raise ValueError(f'bad time_delta type: {type(time_delta)}')
+        self.time_delta = time_delta
+
         if isinstance(data, DGStorage):
             self._storage = data
         else:
             events = data if isinstance(data, list) else read_events(data, **kwargs)
             self._storage = DGStorage(events)
 
-        if not isinstance(time_delta, TimeDeltaDG):
-            raise ValueError(f'bad time_delta type: {type(time_delta)}')
-        self.time_delta = time_delta
-
         self._cache: Dict[str, Any] = {}
 
     def to_events(self) -> List[Event]:
-        r"""Materialize the events in the DGraph.
-
-        Returns:
-            List of events in the current DGraph. Events are ordered by time, but event ordering
-            within a single timestamp is not deterministic.
-        """
+        r"""Materialize the events in the DGraph."""
         return self._storage.to_events(
             start_time=self._cache.get('start_time'),
             end_time=self._cache.get('end_time'),
@@ -46,21 +43,14 @@ class DGraph:
 
     def slice_time(
         self, start_time: Optional[int] = None, end_time: Optional[int] = None
-    ) -> 'DGraph':
-        r"""Extract temporal slice of the dynamic graph between start_time and end_time.
-
-        If not specified, the start_time (resp. end_time) defaults to `self.start_time` (resp. `self.end_time`).
-        The end_time parameter is exclusive, meaning the returned view contains all events up to, but not including
-        those at time end_time. The returned view may or may not have events at the new temporal boundaries.
-
-        Args:
-            start_time (Optional[int]): The start of the temporal slice. If None, slices the graph with no lower bound on time.
-            end_time (Optional[int]): The end of the temporal slice (inclusive). If None, slices the graph with no upper bound on time.
-
-        Returns:
-            DGraph view of events between start and end_time.
-        """
-        new_start_time, new_end_time = self._check_slice_time_args(start_time, end_time)
+    ) -> DGraph:
+        r"""Create and return a new view by slicing temporally (end_time inclusive)."""
+        new_start_time = start_time if start_time is not None else float('-inf')
+        new_end_time = end_time if end_time is not None else float('inf')
+        if new_start_time > new_end_time:
+            raise ValueError(
+                f'start_time ({start_time}) must be <= end_time ({end_time})'
+            )
         new_end_time -= 1  # Because slicing is end range exclusive
 
         dg = DGraph(data=self._storage, time_delta=self.time_delta)
@@ -68,7 +58,6 @@ class DGraph:
 
         if self.start_time is not None and self.start_time > new_start_time:
             new_start_time = self.start_time
-
         if self.end_time is not None and self.end_time < new_end_time:
             new_end_time = self.end_time
 
@@ -82,20 +71,8 @@ class DGraph:
 
         return dg
 
-    def slice_nodes(self, nodes: List[int]) -> 'DGraph':
-        r"""Extract topological slice of the dynamcic graph given the list of nodes.
-
-        The returned view contains events which interact with the list of input nodes. This means
-        the edges with at least one endpoint node in the input list are included in the returned view.
-        The start_time and end_time of the returned view correspond to the minimum (resp. maximum) timestamp
-        in the newly sliced graph (or None if the newly created view is empty).
-
-        Args:
-            nodes (List[int]): The list of node ids to slice from.
-
-        Returns:
-            DGraph copy of events related to the input nodes.
-        """
+    def slice_nodes(self, nodes: List[int]) -> DGraph:
+        r"""Create and return a new view by slicing nodes to include."""
         dg = DGraph(data=self._storage, time_delta=self.time_delta)
 
         if self._cache.get('node_slice') is None:
@@ -137,12 +114,11 @@ class DGraph:
         return dg
 
     def __len__(self) -> int:
-        r"""Returns the number of temporal length of the dynamic graph."""
+        r"""The number of timestamps in the dynamic graph."""
         return self.num_timestamps
 
     def __str__(self) -> str:
-        r"""Returns summary properties of the dynamic graph."""
-        return f'Dynamic Graph Storage Engine ({self._storage.__class__.__name__}), Start Time: {self.start_time}, End Time: {self.end_time}, Nodes: {self.num_nodes}, Edges: {self.num_edges}, Timestamps: {self.num_timestamps}, Time Delta: {self.time_delta}'
+        return f'DGraph(storage={self._storage.__class__.__name__}, time_delta={self.time_delta})'
 
     @property
     def start_time(self) -> Optional[int]:
@@ -213,13 +189,8 @@ class DGraph:
     def node_feats(self) -> Optional[Tensor]:
         r"""The aggregated node features over the dynamic graph.
 
-        Returns a tensor.sparse_coo_tensor of size T x V x d where
-
-        - T = Number of timestamps
-        - V = Number of nodes
-        - d = Node feature dimension
-        or None if there are no node features on the dynamic graph.
-
+        Returns a Tensor.sparse_coo_tensor of size T x V x d_node or None if
+        there are no node features on the dynamic graph.
         """
         if self._cache.get('node_feats') is None:
             self._cache['node_feats'] = self._storage.get_node_feats(
@@ -233,13 +204,8 @@ class DGraph:
     def edge_feats(self) -> Optional[Tensor]:
         r"""The aggregated edge features over the dynamic graph.
 
-        Returns a tensor.sparse_coo_tensor of size T x V x V x d where
-
-        - T = Number of timestamps
-        - V = Number of nodes
-        - d = Edge feature dimension
-
-        or None if there are no edge features on the dynamic graph.
+        Returns a Tensor.sparse_coo_tensor of size T x V x V x d_edge or None if
+        there are no edge features on the dynamic graph.
         """
         if self._cache.get('edge_feats') is None:
             self._cache['edge_feats'] = self._storage.get_edge_feats(
@@ -248,14 +214,3 @@ class DGraph:
                 self._cache.get('node_slice'),
             )
         return self._cache['edge_feats']
-
-    def _check_slice_time_args(
-        self, start_time: Optional[int], end_time: Optional[int]
-    ) -> Tuple[Union[int, float], Union[int, float]]:
-        new_start_time = start_time if start_time is not None else float('-inf')
-        new_end_time = end_time if end_time is not None else float('inf')
-        if new_start_time > new_end_time:
-            raise ValueError(
-                f'Bad slice: start_time must be <= end_time but received: start_time ({new_start_time}) > end_time ({new_end_time})'
-            )
-        return new_start_time, new_end_time
