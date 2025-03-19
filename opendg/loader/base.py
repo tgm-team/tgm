eb3ae0c96604601d0b7b8083a56e502512c4e377
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-19 09:52:18 -0400

    Throw ValueError if trying to iterate by events

--- opendg/loader/base.py
+++ opendg/loader/base.py
@@ -16,22 +16,8 @@ class DGBaseLoader(ABC):
     Raises:
         ValueError: If the batch_unit is not a valid TimeDeltaDG unit.
         ValueError: If the batch_size is not a positive integer.
-        ValueError: If the batch_unit is not ordered and the graph time unit is ordered.
-        ValueError: If both the batch_unit and the graph are not ordered, but the graph TimeDelta
-                    is coarser than the batch TimeDelta. In this case, there is ambiguity in how to yield
-                    events due to loss of information.
-
-    Note:
-        Ordered batch_unit iterates using normal batch size semantics
-        in which case each yielded batch has a constant number of events (except the last batch if drop_last=False).
-            e.g. batch_size=5, batch_unit='r'-> yield 5 events at time
-
-        Unordered batch_unit iterates uses the appropriate temporal unit in which case each yielded
-        batch may have different number of events but has the same temporal length (except the last batch if drop_last=False).
-            e.g. batch_size=5, batch_unit='s'-> yield 5 seconds of data at a time
-
-        When using the ordered batch_unit, the order of yielded _events within the same timestamp
-        is non-deterministic.
+        ValueError: If the batch_unit and dg time unit are not both ordered or both not ordered.
+        ValueError: If the batch_unit and dg time unit are both ordered but the graph is coarser than the batch.
     """
 
     def __init__(
@@ -43,19 +29,20 @@ class DGBaseLoader(ABC):
     ) -> None:
         if batch_size <= 0:
             raise ValueError(f'batch_size must be > 0 but got {batch_size}')
+        if not len(dg):
+            raise ValueError('Cannot iterate an empty DGraph')
 
         self._dg = dg
         self._batch_size = batch_size
         self._batch_unit = batch_unit
         self._drop_last = drop_last
-
         dg_is_ordered = self._dg.time_delta.is_ordered
         batch_unit_is_ordered = self._batch_unit == 'r'
 
         if dg_is_ordered and not batch_unit_is_ordered:
-            raise ValueError(
-                'Cannot use non-ordered batch_unit to iterate a DGraph with ordered time_delta'
-            )
+            raise ValueError('Cannot iterate ordered dg using non-ordered batch_unit')
+        if not dg_is_ordered and batch_unit_is_ordered:
+            raise ValueError('Cannot iterate non-ordered dg using ordered batch_unit')
 
         if not dg_is_ordered and not batch_unit_is_ordered:
             # Check to ensure the graph time unit is smaller (more granular) than batch time unit.
@@ -71,16 +58,8 @@ class DGBaseLoader(ABC):
 
             self._batch_size = int(batch_time_delta.convert(self._dg.time_delta))
 
-        # TODO: Check for time gap?
-        self._iterate_by_events = not dg_is_ordered and batch_unit_is_ordered
-
-        self._idx = 0
-        self._events = []
-        if self._iterate_by_events and len(self._dg):
-            self._events = self._dg._storage.to_events()
-        elif len(self._dg):
-            assert self._dg.start_time is not None
-            self._idx = self._dg.start_time
+        assert self._dg.start_time is not None
+        self._idx = self._dg.start_time
 
     @abstractmethod
     def sample(self, batch: 'DGraph') -> 'DGraph':
@@ -100,12 +79,7 @@ class DGBaseLoader(ABC):
         if self._done_iteration():
             raise StopIteration
 
-        if self._iterate_by_events:
-            events = self._events[self._idx : self._idx + self._batch_size]
-            batch = DGraph(events, time_delta=self._dg.time_delta)
-        else:
-            batch = self._dg.slice_time(self._idx, self._idx + self._batch_size - 1)
-
+        batch = self._dg.slice_time(self._idx, self._idx + self._batch_size - 1)
         self._idx += self._batch_size
         return self.sample(batch)
 
@@ -118,8 +92,5 @@ class DGBaseLoader(ABC):
         else:
             check_idx = self._idx
 
-        if self._iterate_by_events:
-            return check_idx >= len(self._events)
-        else:
-            assert self._dg.end_time is not None
-            return check_idx >= self._dg.end_time + 1
+        assert self._dg.end_time is not None
+        return check_idx >= self._dg.end_time + 1
