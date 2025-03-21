a44d92d2e466ece77ceb2a98270ff9fc237cc221
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-21 10:47:09 -0400

    Update new changes from dataloader

--- opendg/loader/neighbor_loader.py
+++ opendg/loader/neighbor_loader.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
+
 from typing import List
 
 from opendg.graph import DGraph
 from opendg.loader.base import DGBaseLoader
-from opendg.timedelta import TimeDeltaUnit
 
 
 class DGNeighborLoader(DGBaseLoader):
@@ -10,32 +11,16 @@ class DGNeighborLoader(DGBaseLoader):
 
     Args:
         dg (DGraph): The dynamic graph to iterate.
-        num_nbrs (List[int]): The number of hops to sample, and the number of neighbors to sample at each hop (-1 to keep all)
+        num_nbrs (List[int]): Number of neighbors to sample at each hop (-1 to keep all)
         batch_size (int): The batch size to yield at each iteration.
         batch_unit (str): The unit corresponding to the batch_size.
         drop_last (bool): Set to True to drop the last incomplete batch.
+        materialize (bool): If True (default), auto-materialize the DGraph before yielding a batch.
 
     Raises:
         ValueError: If the num_nbrs list is empty.
-        ValueError: If any of the values in num_nbrs is not a positive integer or -1.
-        ValueError: If the batch_unit is not a valid TimeDeltaUnit type.
-        ValueError: If the batch_size is not a positive integer.
-        ValueError: If the batch_unit is not TimeDeltaUnit.ORDERED and the graph is TimeDeltaUnit.ORDERED.
-        ValueError: If both the batch_unit and the graph are not TimeDeltaUnit.ORDERED, but the graph TimeDelta
-                    is coarser than the batch TimeDelta. In this case, there is ambiguity in how to yield
-                    events due to loss of information.
-
-    Note:
-        Ordered batch_unit ('TimeDeltaUnit.ORDERED) iterates using normal batch size semantics
-        in which case each yielded batch has a constant number of events (except the last batch if drop_last=False).
-            e.g. batch_size=5, batch_unit=TimeDeltaUnit.ORDERED -> yield 5 events at time
-
-        Unordered batch_unit iterates uses the appropriate temporal unit in which case each yielded
-        batch may have different number of events but has the same temporal length (except the last batch if drop_last=False).
-            e.g. batch_size=5, batch_unit=TimeDeltaUnit.SECONDS -> yield 5 seconds of data at a time
-
-        When using the ordered batch_unit, the order of yielded _events within the same timestamp
-        is non-deterministic.
+        ValueError: If the batch_unit and dg time unit are not both ordered or both not ordered.
+        ValueError: If the batch_unit and dg time unit are both ordered but the graph is coarser than the batch.
     """
 
     def __init__(
@@ -43,38 +28,26 @@ class DGNeighborLoader(DGBaseLoader):
         dg: DGraph,
         num_nbrs: List[int],
         batch_size: int = 1,
-        batch_unit: str = TimeDeltaUnit.ORDERED,
+        batch_unit: str = 'r',
         drop_last: bool = False,
+        materialize: bool = True,
     ) -> None:
         if not len(num_nbrs):
             raise ValueError('num_nbrs must be non-empty')
-
         if not all([isinstance(x, int) and (x == -1 or x > 0) for x in num_nbrs]):
-            raise ValueError(
-                'Each value in num_nbrs must be a positive integer or -1 to keep all neighbors'
-            )
+            raise ValueError('Each value in num_nbrs must be a positive integer or -1')
         self._num_nbrs = num_nbrs
-        super().__init__(dg, batch_size, batch_unit, drop_last)
+        super().__init__(dg, batch_size, batch_unit, drop_last, materialize)
 
     @property
     def num_hops(self) -> int:
-        r"""The number of hops that the sampler is using."""
         return len(self.num_nbrs)
 
     @property
     def num_nbrs(self) -> List[int]:
-        r"""The number of hops to sample, and the number of neighbors to sample at each hop."""
         return self._num_nbrs
 
-    def sample(self, batch: 'DGraph') -> 'DGraph':
-        r"""DGDataLoader performs no subsampling. Returns the full batch.
-
-        Args:
-            batch (DGraph): Incoming batch of data. May not be materialized.
-
-        Returns:
-            (DGraph): The input batch of data.
-        """
+    def pre_yield(self, batch: DGraph) -> DGraph:
         # TODO: Need a way to easily get nodes in the graph
         batch_nodes = self._dg._storage.get_nodes(batch.start_time, batch.end_time)
 
@@ -82,7 +55,7 @@ class DGNeighborLoader(DGBaseLoader):
         nbrs = self._dg._storage.get_nbrs(
             seed_nodes=list(batch_nodes),
             num_nbrs=self.num_nbrs,
-            end_time=batch.start_time,
+            end_time=batch.end_time,
         )
         # TODO: Extract subgraph with nbrs
         return batch
--- test/test_loader/test_neighbor_loader.py
+++ test/test_loader/test_neighbor_loader.py
@@ -6,30 +6,28 @@ from opendg.loader import DGNeighborLoader
 
 
 def test_init_bad_num_nbrs():
-    events = [EdgeEvent(time=1, edge=(2, 3))]
+    events = [EdgeEvent(t=1, src=2, dst=3)]
     dg = DGraph(events)
     with pytest.raises(ValueError):
         _ = DGNeighborLoader(dg, num_nbrs=[])
-
     with pytest.raises(ValueError):
         _ = DGNeighborLoader(dg, num_nbrs=[-2, 2])
 
 
 def test_iteration_with_sampling():
     events = [
-        EdgeEvent(time=0, edge=(1, 2)),
-        EdgeEvent(time=1, edge=(2, 3)),
-        EdgeEvent(time=2, edge=(3, 4)),
-        EdgeEvent(time=3, edge=(4, 5)),
-        EdgeEvent(time=4, edge=(5, 6)),
-        EdgeEvent(time=5, edge=(6, 7)),
-        EdgeEvent(time=6, edge=(7, 8)),
-        EdgeEvent(time=7, edge=(8, 9)),
-        EdgeEvent(time=8, edge=(9, 10)),
-        EdgeEvent(time=9, edge=(10, 11)),
+        EdgeEvent(t=0, src=1, dst=2),
+        EdgeEvent(t=1, src=2, dst=3),
+        EdgeEvent(t=2, src=3, dst=4),
+        EdgeEvent(t=3, src=4, dst=5),
+        EdgeEvent(t=4, src=5, dst=6),
+        EdgeEvent(t=5, src=6, dst=7),
+        EdgeEvent(t=6, src=7, dst=8),
+        EdgeEvent(t=7, src=8, dst=9),
+        EdgeEvent(t=8, src=9, dst=10),
+        EdgeEvent(t=9, src=10, dst=11),
     ]
     dg = DGraph(events)
-
     loader = DGNeighborLoader(dg, num_nbrs=[1])
     for batch in loader:
         continue
