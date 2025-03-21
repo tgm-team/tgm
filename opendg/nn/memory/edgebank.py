24e685e35546cddb07ddd28853fb4f92a5b114a3
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-20 22:44:53 -0400

    Pure refactors

--- opendg/nn/memory/edgebank.py
+++ opendg/nn/memory/edgebank.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Dict, Literal, Tuple
 
 import numpy as np
 
@@ -9,54 +9,42 @@ class EdgeBankPredictor:
         src: np.ndarray,
         dst: np.ndarray,
         ts: np.ndarray,
-        memory_mode: Literal[
-            'unlimited', 'fixed_time_window'
-        ] = 'unlimited',  # could be `unlimited` or `fixed_time_window`
-        time_window_ratio: float = 0.15,
+        memory_mode: Literal['unlimited', 'fixed'] = 'unlimited',
+        window_ratio: float = 0.15,
         pos_prob: float = 1.0,
     ) -> None:
-        r"""Intialize edgebank and specify the memory mode.
+        r"""Edgebank link predictor with fixed or unlimited memory. Reference: https://arxiv.org/abs/2207.10128.
 
         Args:
             src(np.ndarray): source node id of the edges for initialization
             dst(np.ndarray): destination node id of the edges for initialization
             ts(np.ndarray): timestamp of the edges for initialization
-            memory_mode(str): 'unlimited' or 'fixed_time_window'
-            time_window_ratio(float): the ratio of the time window length to the total time length
+            memory_mode(str): 'unlimited' or 'fixed'
+            window_ratio(float): the ratio of the time window length to the total time length
             pos_prob(float): the probability of the link existence for the edges in memory.
         """
-        if memory_mode not in ['unlimited', 'fixed_time_window']:
-            raise ValueError(f'Invalid memory mode for EdgeBank, expected one of ["unlimited", "fixed"]')
-
-        if (time_window_ratio <= 0.0) or (time_window_ratio > 1.0):
-            raise ValueError(
-                f'Invalid time window ratio for EdgeBank, must be in (0,1]'
-            )
-
+        if memory_mode not in ['unlimited', 'fixed']:
+            raise ValueError(f'memory_mode must be "unlimited" or "fixed"')
+        if not 0 < window_ratio <= 1.0:
+            raise ValueError(f'Window ratio must be in (0, 1]')
         self._check_input_data(src, dst, ts)
 
         self.memory_mode = memory_mode
-
-        self.cur_t = ts.max()
-        self._time_window_ratio = time_window_ratio
-
-        if self.memory_mode == 'fixed_time_window':
-            # determine the time window size based on ratio from the given src, dst, and ts for initialization
-            duration = ts.max() - ts.min()
-            self.prev_t = ts.min() + duration * (
-                1.0 - time_window_ratio
-            )  # the time windows starts from the last ratio% of time
-        else:
-            self.prev_t = ts.min()
-        self.duration = self.cur_t - self.prev_t
-
-        self.memory: dict[tuple[int, int], int] = {}  # {(u,v):1}
         self.pos_prob = pos_prob
+        self._window_ratio = window_ratio
+        self._fixed_memory = memory_mode == 'fixed'
+
+        self._window_start = ts.min()
+        if self._fixed_memory:
+            self._window_start = ts.min() + (ts.max() - ts.min()) * (1.0 - window_ratio)
+        self._window_end = ts.max()
+        self._duration = self._window_end - self._window_start
+
+        self.memory: Dict[Tuple[int, int], int] = {}
         self.update_memory(src, dst, ts)
 
     def update_memory(self, src: np.ndarray, dst: np.ndarray, ts: np.ndarray) -> None:
-        r"""Generate the current and correct state of the memory with the observed edges so far.
-        note that historical edges may include training, validation, and already observed test edges.
+        r"""Update Edgebank memory with a batch of edges.
 
         Args:
             src(np.ndarray): source node id of the edges.
@@ -64,74 +52,10 @@ class EdgeBankPredictor:
             ts(np.ndarray): timestamp of the edges.
         """
         self._check_input_data(src, dst, ts)
-
-        if self.memory_mode == 'unlimited':
-            self._update_unlimited_memory(src, dst)  # ignores time
-        elif self.memory_mode == 'fixed_time_window':
-            self._update_time_window_memory(src, dst, ts)
-        else:
-            raise ValueError('Invalide memory mode!')
-
-    @property
-    def start_time(self) -> int:
-        r"""The start of time window for edgebank.
-
-        Returns:
-            int: start of time window.
-        """
-        return self.prev_t
-
-    @property
-    def end_time(self) -> int:
-        r"""The end of time window for edgebank.
-
-        Returns:
-            int: end of time window.
-        """
-        return self.cur_t
-
-    @property
-    def time_window_ratio(self) -> float:
-        r"""The ratio of the time window length to the total time length.
-
-        Returns:
-            float: the ratio of the time window length to the total time length.
-        """
-        return self._time_window_ratio
-
-    def _check_input_data(
-        self, src: np.ndarray, dst: np.ndarray, ts: np.ndarray
-    ) -> None:
-        if not (type(src) == type(dst) == type(ts) == np.ndarray):
-            raise TypeError(
-                f'src, dst, and ts must be numpy, got {type(src)}, {type(dst)}, {type(ts)}'
-            )
-
-        if not (len(src) == len(dst) == len(ts)):
-            raise ValueError(
-                f'src, dst, and ts must have the same length, got {len(src)}, {len(dst)}, {len(ts)}'
-            )
-        if len(src) == 0:
-            raise ValueError('src, dst, and ts must have at least one element')
-
-    def _update_unlimited_memory(
-        self, update_src: np.ndarray, update_dst: np.ndarray
-    ) -> None:
-        for src, dst in zip(update_src, update_dst):
-            if (src, dst) not in self.memory:
-                self.memory[(src, dst)] = 1
-
-    def _update_time_window_memory(
-        self, update_src: np.ndarray, update_dst: np.ndarray, update_ts: np.ndarray
-    ) -> None:
-        # * update the memory if it is not empty
-        if update_ts.max() > self.cur_t:
-            self.cur_t = update_ts.max()
-            self.prev_t = self.cur_t - self.duration
-
-        # * add new edges to the time window
-        for src, dst, ts in zip(update_src, update_dst, update_ts):
-            self.memory[(src, dst)] = ts
+        self._window_end = max(self._window_end, ts.max())
+        self._window_start = self._window_end - self._duration
+        for src_, dst_, ts_ in zip(src, dst, ts):
+            self.memory[(src_, dst_)] = ts_
 
     def predict_link(self, query_src: np.ndarray, query_dst: np.ndarray) -> np.ndarray:
         r"""Predict the probability from query src,dst pair given the current memory,
@@ -145,14 +69,32 @@ class EdgeBankPredictor:
             pred(np.ndarray): the prediction for all query edges.
         """
         pred = np.zeros(len(query_src))
-        idx = 0
-        for src, dst in zip(query_src, query_dst):
-            if (src, dst) in self.memory:
-                if self.memory_mode == 'fixed_time_window':
-                    if self.memory[(src, dst)] >= self.prev_t:
-                        pred[idx] = self.pos_prob
-                else:
-                    pred[idx] = self.pos_prob
-            idx += 1
-
+        for i, edge in enumerate(zip(query_src, query_dst)):
+            if edge in self.memory:
+                hit = not self._fixed_memory
+                hit |= self._fixed_memory and self.memory[edge] >= self.window_start
+                if hit:
+                    pred[i] = self.pos_prob
         return pred
+
+    @property
+    def window_start(self) -> int:
+        return self._window_start
+
+    @property
+    def window_end(self) -> int:
+        return self._window_end
+
+    @property
+    def window_ratio(self) -> float:
+        return self._window_ratio
+
+    def _check_input_data(
+        self, src: np.ndarray, dst: np.ndarray, ts: np.ndarray
+    ) -> None:
+        if not (type(src) == type(dst) == type(ts) == np.ndarray):
+            raise TypeError('src, dst, ts must all be np.ndarray')
+        if not (len(src) == len(dst) == len(ts)):
+            raise ValueError(f'mismatch src:{len(src)}, dst: {len(dst)}, ts: {len(ts)}')
+        if len(src) == 0:
+            raise ValueError('src, dst, and ts must have at least one element')
--- test/test_model/test_edgebank.py
+++ test/test_model/test_edgebank.py
@@ -53,7 +53,7 @@ def test_no_loader_fixed_time_window():
     dsts = edge_index[:, 1]
     ts = edge_index[:, 2]
 
-    MEMORY_MODE = 'fixed_time_window'
+    MEMORY_MODE = 'fixed'
     TIME_WINDOW_RATIO = 0.5
 
     edgebank = EdgeBankPredictor(
@@ -61,7 +61,7 @@ def test_no_loader_fixed_time_window():
         np.asarray(dsts),
         np.asarray(ts),
         memory_mode=MEMORY_MODE,
-        time_window_ratio=TIME_WINDOW_RATIO,
+        window_ratio=TIME_WINDOW_RATIO,
     )
 
     pred = edgebank.predict_link(np.array([4]), np.array([5]))
@@ -100,22 +100,22 @@ def test_edgebank_arguments():
     dsts = edge_index[:, 1]
     ts = edge_index[:, 2]
 
-    MEMORY_MODE = 'fixed_time_window'
+    MEMORY_MODE = 'fixed'
     pos_prob = 0.01
-    time_window_ratio = 0.15
+    window_ratio = 0.15
 
     edgebank = EdgeBankPredictor(
         srcs,
         dsts,
         ts,
         memory_mode=MEMORY_MODE,
-        time_window_ratio=time_window_ratio,
+        window_ratio=window_ratio,
         pos_prob=pos_prob,
     )
 
-    assert edgebank.time_window_ratio == 0.15
-    assert edgebank.start_time == 5.25
-    assert edgebank.end_time == 6
+    assert edgebank.window_ratio == 0.15
+    assert edgebank.window_start == 5.25
+    assert edgebank.window_end == 6
 
     pred = edgebank.predict_link(np.array([6]), np.array([7]))
     assert pred[0] == pos_prob
@@ -125,35 +125,26 @@ def test_edgebank_arguments():
         dsts,
         ts,
     )
-    assert edgebank.start_time == 1
-    assert edgebank.end_time == 6
+    assert edgebank.window_start == 1
+    assert edgebank.window_end == 6
 
     pred = edgebank.predict_link(np.array([1]), np.array([2]))
     assert pred[0] == 1
 
-    with pytest.raises(
-        ValueError, match='src, dst, and ts must have the same length, got 0, 0, 1'
-    ):
+    with pytest.raises(ValueError):
         edgebank.update_memory(np.array([]), np.array([]), np.array([1]))
 
-    with pytest.raises(
-        ValueError, match='src, dst, and ts must have at least one element'
-    ):
+    with pytest.raises(ValueError):
         edgebank.update_memory(np.array([]), np.array([]), np.array([]))
 
-    with pytest.raises(
-        ValueError, match='src, dst, and ts must have at least one element'
-    ):
+    with pytest.raises(ValueError):
         edgebank = EdgeBankPredictor(
             np.array([]),
             np.array([]),
             np.array([]),
         )
 
-    with pytest.raises(
-        TypeError,
-        match="src, dst, and ts must be numpy, got <class 'int'>, <class 'int'>, <class 'int'>",
-    ):
+    with pytest.raises(TypeError):
         edgebank = EdgeBankPredictor(
             1,
             2,
