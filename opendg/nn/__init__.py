1143a822e65b17d46088cbffbc3e74451f56deb3
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-21 18:36:20 -0400

    Add time2vec

--- opendg/nn/__init__.py
+++ opendg/nn/__init__.py
@@ -1,3 +1,4 @@
 from .memory import EdgeBankPredictor
+from .time_encoding import Time2Vec
 
-__all__ = ['EdgeBankPredictor']
+__all__ = ['EdgeBankPredictor', 'Time2Vec']
---
+++ opendg/nn/time_encoding.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Time2Vec(nn.Module):
+    def __init__(self, time_dim: int) -> None:
+        """Time encoder representation.
+
+        Args:
+            time_dim (int): The dimension of time encodings.
+        """
+        super().__init__()
+        self.w = torch.nn.Linear(1, time_dim)
+
+        # Initialization from: https://github.com/yule-BUAA/DyGLib/blob/master/models/modules.py
+        w = (1 / 10 ** np.linspace(0, 9, time_dim)).reshape(time_dim, 1)
+        self.w.weight = torch.nn.Parameter(torch.from_numpy(w).float())
+        self.w.bias = torch.nn.Parameter(torch.zeros(time_dim))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.unsqueeze(dim=-1)  # (batch_size, seq_len, 1)
+        return torch.cos(self.w(x))
