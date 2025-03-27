615bf6e63dda8435c366b9636afe1d5c14481ef2
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-03-27 13:07:58 -0400

    Update edgebank usage

--- examples/edgebank.py
+++ examples/edgebank.py
@@ -32,14 +32,11 @@ def eval(loader: DGBaseLoader, model: EdgeBankPredictor) -> float:
     perf_list = []
     for batch in loader:
         # where is the negative batch list?
-        src, pos_dst, neg_dst, time, features = batch
-
-        # TODO: Make model use __call__ for simpler api
-        prob_pos, prob_neg = model(batch)
+        src, pos_dst, neg_dst, time, _ = batch
+        prob_pos = model(src, pos_dst)
+        prob_neg = model(src, neg_dst)
         perf_list.append(prob_pos - prob_neg)  # TODO: MRR eval
-
-        # Need a way to extract from tensors
-        model.update_memory(src=src, dst=pos_dst, ts=time)
+        model.update(src, pos_dst, time)
     return np.mean(perf_list)
 
 
@@ -52,12 +49,12 @@ def run(args: argparse.Namespace) -> None:
     # TODO: Would be convenient to have a dispatcher based on sampling_type
     val_loader = DGDataLoader(val_dg, batch_size=args.bsize)
 
-    # TODO: Need a way to extract src, dst, ts edges without the features
     # device = torch.device(f'cuda:{args.gpu}' if args.gpu >= 0 else 'cpu')
-    model = EdgeBankPredictor(  # TODO: Why doesn't edgebank work with tensors
-        src=None,
-        dst=None,
-        ts=None,
+    src, dst, ts, _ = train_dg.materialize(materialize_features=False)
+    model = EdgeBankPredictor(
+        src,
+        dst,
+        ts,
         memory_mode=args.memory_mode,
         window_ratio=args.window_ratio,
         pos_prob=args.pos_prob,
