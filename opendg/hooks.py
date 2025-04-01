300d103d487097cbd942b20a043e76b27009d83b
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2025-04-01 15:20:49 -0400

    first draft recency loader

--- opendg/hooks.py
+++ opendg/hooks.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import List, Protocol
 
 import torch
-
+from queue import Queue
 from opendg._storage import DGSliceTracker
 from opendg.graph import DGBatch, DGraph
 
@@ -77,4 +77,90 @@ class NeighborSamplerHook:
         dg._slice = DGSliceTracker(
             end_idx=dg._slice.start_idx, node_slice=temporal_nbrhood
         )
+        return dg.materialize()
+
+
+
+
+
+
+class RecencyNeighborSamplerHook:
+    r"""Load neighbors from DGraph using a recency sampling strategy where each node maintains a fixed number of most recent neighbors. 
+
+    Args:
+        num_nbrs (List[int]): Number of neighbors to sample at each hop, also indicates the max number of neighbors to keep in the recency sampling strategy. (int must be > 0)
+        return_nbr_graph (bool): Whether to return the neighbor graph in DGBatch or not. Default is False, if True, the DGBatch edges in this case will not be sorted chronologically.
+        **kwargs (Any): Additional arguments to the DGDataLoader
+
+    Raises:
+        ValueError: If the num_nbrs list is empty.
+    """
+
+    def __init__(self, 
+                 num_nbrs: List[int], 
+                 return_nbr_graph: bool = True,) -> None:
+        if not len(num_nbrs):
+            raise ValueError('num_nbrs must be non-empty')
+        if not all([isinstance(x, int) and (x > 0) for x in num_nbrs]):
+            raise ValueError('Each value in num_nbrs must be a positive integer')
+        if len(num_nbrs) > 1:
+            raise ValueError('RecencyNeighborSamplerHook only supports 1 hop for now')
+        self._num_nbrs = num_nbrs
+        self.return_nbr_graph = return_nbr_graph
+        self._nbr_dict = {} # {node_id: queue(maxlen=num_nbr)} # one queue for dst id, time, edge_feats
+
+
+    @property
+    def num_nbrs(self) -> List[int]:
+        return self._num_nbrs
+
+    def __call__(self, dg: DGraph) -> DGBatch:
+
+        batch = dg.materialize()
+        """
+        src: Tensor
+        dst: Tensor
+        time: Tensor
+        node_feats: Optional[Tensor] = None
+        edge_feats: Optional[Tensor] = None
+        """
+        src = batch.src
+        dst = batch.dst
+        nids = torch.unique(torch.cat((src, dst)))
+        out_nbrs = {}
+
+        # retrieve recent neighbors for each node
+        for node in nids.tolist():
+            if node not in self._nbr_dict:
+                if (batch.edge_feats is not None):
+                    self._nbr_dict[node] = [Queue(maxsize=self._num_nbrs[0]), Queue(maxsize=self._num_nbrs[0]), Queue(maxsize=self._num_nbrs[0])]
+                else:
+                    self._nbr_dict[node] = [Queue(maxsize=self._num_nbrs[0]), Queue(maxsize=self._num_nbrs[0])]                
+            out_nbrs[node] = [list(self._nbr_dict[node][i].queue) for i in len(self._nbr_dict[node])]  #(dst,time, edge_feats)
+        
+        # add new neighbors to the recency dict
+        #! do we need it to be undirected? don't think so, thus only adding src->dst
+        for i in range(src.size(0)):
+            src_nbr = src[i].item()
+            if (self._nbr_dict[src_nbr][0].full()):
+                for k in range(len(self._nbr_dict[src_nbr])):
+                    self._nbr_dict[src_nbr][k].get()
+
+            self._nbr_dict[src_nbr][0].put(dst[i])
+            self._nbr_dict[src_nbr][1].put(batch.time[i])
+            if (batch.edge_feats is not None):
+                self._nbr_dict[src_nbr][2].put(batch.edge_feats[i])  
+
+        # create the new batch              
+        if self.return_nbr_graph:
+            src_nbr = []
+
+        else:
+            return 
+
+
+
+
+
+
         return dg.materialize()
---
+++ test/test_hooks/test_recency_nbr_hook.py
@@ -0,0 +1,32 @@
+import pytest
+
+from opendg.events import EdgeEvent, NodeEvent
+from opendg.graph import DGBatch, DGraph
+from opendg.hooks import RecencyNeighborSamplerHook
+
+
+@pytest.fixture
+def events():
+    return [
+        EdgeEvent(t=1, src=2, dst=2),
+        EdgeEvent(t=5, src=2, dst=4),
+        EdgeEvent(t=20, src=1, dst=8),
+    ]
+
+
+def test_bad_neighbor_sampler_init():
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[0])
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[-1])
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[1, 2])
+
+
+def test_neighbor_sampler_hook(events):
+    dg = DGraph(events)
+    hook = RecencyNeighborSamplerHook(num_nodes=dg.num_nodes, size=3)
+    batch = hook(dg)
+    assert isinstance(batch, DGBatch)
+
+    # TODO: Add logic for testing
