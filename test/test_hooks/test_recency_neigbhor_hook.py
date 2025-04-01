9089abdea1c12df4ff080a694246127e946ec15e
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-04-01 15:19:34 -0400

    WIP

--- test/test_hooks/test_recency_neigbhor_hook.py
+++ test/test_hooks/test_recency_neigbhor_hook.py
@@ -1,7 +1,6 @@
 import pytest
 
 from opendg.events import EdgeEvent, NodeEvent
-from opendg.graph import DGBatch, DGraph
 from opendg.hooks import LastNeighborHook
 
 
@@ -25,9 +24,49 @@ def test_bad_neighbor_sampler_init():
 
 
 def test_neighbor_sampler_hook(events):
-    dg = DGraph(events)
-    hook = LastNeighborHook(num_nodes=dg.num_nodes, size=3)
-    batch = hook(dg)
-    assert isinstance(batch, DGBatch)
+    import time
 
-    # TODO: Add logic for testing
+    import torch
+
+    def _update(src, dst):
+        new_nbrs.fill_(-1)
+        for i in range(len(src)):
+            new_nbrs[i, src[i]] = dst[i]
+            new_nbrs[i, dst[i]] = src[i]
+        # print(new_nbrs)
+        # print((new_nbrs >= 0).sum(dim=1))
+        torch.cat([nbrs, new_nbrs])
+        # all_nbr = torch.gather(all_nbr, 1, all_nbr.ne(-1).argsort(dim=1, stable=True))
+        return nbrs
+        # return all_nbr[:, -k:]
+
+    from collections import deque
+
+    num_nodes = [1000, 10_000, 100_000, 1_000_000]
+    ks = [16, 32, 64]
+    bs = [256, 512, 1024]
+    print('\n')
+
+    for num_node in num_nodes:
+        for k in ks:
+            for b in bs:
+                print(f'Nodes: {num_node}', end='')
+                print(f'\tK: {k}', end='')
+                print(f'\tBatchSize: {b}', end='')
+
+                qs = [deque(maxlen=k)] * num_node
+                nbrs = torch.full((num_node, k), -1, dtype=torch.long)
+                src = torch.randint(low=0, high=num_node - 1, size=(b,))
+                dst = torch.randint(low=0, high=num_node - 1, size=(b,))
+
+                st = time.perf_counter()
+                for _ in range(10):
+                    unique = torch.cat([src, dst]).unique()
+                    for i in range(len(src)):
+                        qs[src[i]].append(dst[i])
+                        qs[dst[i]].append(src[i])
+                    for u in unique:
+                        nbrs[u] = torch.tensor(qs[u])
+                et = time.perf_counter()
+                throughput = b * 10 / (et - st)
+                print(f'\t {throughput:.2f} updates/s')
