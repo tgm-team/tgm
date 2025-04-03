e8bde3006f8c472ea2413785581f9443743bda97
shenyang(Andy) Huang <shenyang.huang@mail.mcgill.ca>
2025-04-03 14:20:42 -0400

    Recency Hook  (#172)
    * first draft recency loader
    
    * first draft recency loader
    
    * adding first draft for recency loader
    
    * Recency Cleanup (#181)
    
    * Run pre-commit
    
    * Mock perf
    
    * Switch from Queue to Deque
    
    * Minor refactors
    
    * Refactor
    
    * Initialize all nbr memory from start
    
    * Clean up example
    
    * Fix test
    
    * Update docs
    
    ---------
    
    Co-authored-by: Jacob Chmura <jacobpaul.chmura@gmail.com>

---
+++ test/test_hooks/test_recency_nbr_hook.py
@@ -0,0 +1,32 @@
+import pytest
+
+from opendg.events import EdgeEvent
+from opendg.graph import DGBatch, DGraph
+from opendg.hooks import RecencyNeighborSamplerHook
+
+
+@pytest.fixture
+def events():
+    return [
+        EdgeEvent(t=1, src=1, dst=10),
+        EdgeEvent(t=1, src=1, dst=11),
+        EdgeEvent(t=2, src=1, dst=12),
+        EdgeEvent(t=2, src=1, dst=13),
+    ]
+
+
+def test_bad_neighbor_sampler_init():
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[0], num_nodes=2)
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[-1], num_nodes=2)
+    with pytest.raises(ValueError):
+        RecencyNeighborSamplerHook(num_nbrs=[1, 2], num_nodes=2)
+
+
+def test_neighbor_sampler_hook(events):
+    dg = DGraph(events)
+    hook = RecencyNeighborSamplerHook(num_nbrs=[2], num_nodes=dg.num_nodes)
+    batch = hook(dg)
+    # TODO: Add logic for testing
+    assert isinstance(batch, DGBatch)
