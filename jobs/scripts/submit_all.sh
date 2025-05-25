4d440080c6a0c27520c8d0999d5af63459904e0e
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-05-25 13:49:55 -0400

    Even more time on the jobs

--- jobs/scripts/submit_all.sh
+++ jobs/scripts/submit_all.sh
@@ -17,16 +17,16 @@ get_slurm_resources() {
             echo "--partition=main --cpus-per-task=2 --mem=4G --time=0:10:00"
             ;;
         tgat)
-            echo "--partition=main --cpus-per-task=2 --mem=8G --time=1:00:00 --gres=gpu:a100l:1"
+            echo "--partition=main --cpus-per-task=2 --mem=8G --time=3:00:00 --gres=gpu:a100l:1"
             ;;
         tgn)
-            echo "--partition=main --cpus-per-task=2 --mem=8G --time=1:00:00 --gres=gpu:a100l:1"
+            echo "--partition=main --cpus-per-task=2 --mem=8G --time=3:00:00 --gres=gpu:a100l:1"
             ;;
         gcn)
-            echo "--partition=main --cpus-per-task=2 --mem=4G --time=1:00:00 --gres=gpu:a100l:1"
+            echo "--partition=main --cpus-per-task=2 --mem=4G --time=3:00:00 --gres=gpu:a100l:1"
             ;;
         gclstm)
-            echo "--partition=main --cpus-per-task=2 --mem=4G --time=1:00:00 --gres=gpu:a100l:1"
+            echo "--partition=main --cpus-per-task=2 --mem=4G --time=3:00:00 --gres=gpu:a100l:1"
             ;;
         *)
             echo "--partition=main --cpus-per-task=2 --mem=4G --time=1:00:00"
