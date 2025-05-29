ac7ab8e26f9bcab53ccca35fa55667c1781f50e3
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-05-29 11:39:21 -0400

    Add DGData Object

---
+++ opendg/data.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+import torch
+
+
+@dataclass
+class DGData:
+    r"""Bundles dynamic graph data to be forwarded to DGStorage."""
+
+    edge_index: torch.Tensor  # [num_edges, 2]
+    timestamps: torch.Tensor  # [num_edges]
+    edge_feats: torch.Tensor | None = None  # [num_edges, D_edge]
+    node_feats: torch.Tensor | None = None  # [num_nodes, D_node]
+
+    def __post_init__(self) -> None:
+        # Validate edge index
+        if not isinstance(self.edge_index, torch.Tensor):
+            raise TypeError('edge_index must be a torch.Tensor')
+        if self.edge_index.ndim != 2 or self.edge_index.shape[1] != 2:
+            raise ValueError('edge_index must have shape [num_edges, 2]')
+
+        num_edges = self.edge_index.shape[1]
+
+        # Validate edge features
+        if self.edge_feats is not None:
+            if not isinstance(self.edge_feats, torch.Tensor):
+                raise TypeError('edge_feats must be a torch.Tensor')
+            if self.edge_feats.shape[0] != num_edges:
+                raise ValueError('edge_feats must have shape [num_edges, D_edge]')
+
+        # Validate node features
+        if self.node_feats is not None:
+            if not isinstance(self.node_feats, torch.Tensor):
+                raise TypeError('node_feats must be a torch.Tensor')
+
+        # Validate timestamps
+        if not isinstance(self.timestamps, torch.Tensor):
+            raise TypeError('timestamps must be a torch.Tensor')
+        if self.timestamps.ndim != 1 or self.timestamps.shape[0] != num_edges:
+            raise ValueError('timestamps must have shape [num_edges]')
+
+        if num_edges > 1:
+            if self.timestamps[0] > self.timestamps[-1]:
+                self._sort_by_timestamps()
+            elif not torch.all(self.timestamps[:-1] <= self.timestamps[1:]):
+                self._sort_by_timestamps()
+
+    def _sort_by_timestamps(self) -> None:
+        sorted_idx = torch.argsort(self.timestamps)
+        self.timestamps = self.timestamps[sorted_idx]
+        self.edge_index = self.edge_index[sorted_idx]
+        if self.edge_feats is not None:
+            self.edge_feats = self.edge_feats[sorted_idx]
---
+++ test/test_data.py
@@ -0,0 +1,74 @@
+import pytest
+import torch
+
+from opendg.data import DGData
+
+
+def test_init_dg_data_no_feats():
+    edge_index = torch.Tensor([[2, 3], [10, 20]])
+    timestamps = torch.Tensor([1, 5])
+    data = DGData(edge_index, timestamps)
+
+    torch.testing.assert_close(data.edge_index, edge_index)
+    torch.testing.assert_close(data.timestamps, timestamps)
+    assert data.edge_feats is None
+    assert data.node_feats is None
+
+
+def test_init_dg_data_with_feats():
+    edge_index = torch.Tensor([[2, 3], [10, 20]])
+    timestamps = torch.Tensor([1, 5])
+    edge_feats = torch.rand(2, 5)
+    node_feats = torch.rand(100, 10)
+    data = DGData(edge_index, timestamps, edge_feats, node_feats)
+
+    torch.testing.assert_close(data.edge_index, edge_index)
+    torch.testing.assert_close(data.timestamps, timestamps)
+    torch.testing.assert_close(data.edge_feats, edge_feats)
+    torch.testing.assert_close(data.node_feats, node_feats)
+
+
+def test_init_dg_data_sort_required():
+    edge_index = torch.Tensor([[2, 3], [10, 20]])
+    timestamps = torch.Tensor([5, 1])
+    edge_feats = torch.rand(2, 5)
+    node_feats = torch.rand(100, 10)
+    data = DGData(edge_index, timestamps, edge_feats, node_feats)
+
+    # Expected sort
+    exp_edge_index = torch.Tensor([[10, 20], [2, 3]])
+    exp_timestamps = torch.Tensor([1, 5])
+    exp_edge_feats = torch.Tensor([edge_feats[1].tolist(), edge_feats[0].tolist()])
+
+    torch.testing.assert_close(data.edge_index, exp_edge_index)
+    torch.testing.assert_close(data.timestamps, exp_timestamps)
+    torch.testing.assert_close(data.edge_feats, exp_edge_feats)
+    torch.testing.assert_close(data.node_feats, node_feats)
+
+
+def test_init_dg_data_bad_args():
+    edge_index = torch.Tensor([[2, 3], [10, 20]])
+    timestamps = torch.Tensor([5, 1])
+    edge_feats = torch.rand(2, 5)
+
+    # Bad types
+    with pytest.raises(TypeError):
+        _ = DGData('foo', timestamps)
+    with pytest.raises(TypeError):
+        _ = DGData(edge_index, 'foo')
+    with pytest.raises(TypeError):
+        _ = DGData(edge_index, timestamps, 'foo')
+    with pytest.raises(TypeError):
+        _ = DGData(edge_index, timestamps, edge_feats, 'foo')
+
+    # Bad shapes
+    with pytest.raises(ValueError):
+        _ = DGData(torch.Tensor([1, 2]), timestamps)
+    with pytest.raises(ValueError):
+        _ = DGData(torch.Tensor([[1, 2, 3], [10, 20, 30]]), timestamps)
+    with pytest.raises(ValueError):
+        _ = DGData(edge_index, torch.Tensor([1]))
+    with pytest.raises(ValueError):
+        _ = DGData(edge_index, timestamps, torch.rand(1, 5))
+    with pytest.raises(ValueError):
+        _ = DGData(edge_index, timestamps, torch.rand(1, 5, 5))
