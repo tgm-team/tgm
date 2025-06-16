d1837ac389a4105ceed522d1fe0204e4d34282ee
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-06-16 18:03:25 -0400

    Add unit tests

--- test/test_hooks/test_deduplication_hook.py
+++ test/test_hooks/test_deduplication_hook.py
@@ -2,14 +2,16 @@ import pytest
 import torch
 
 from tgm.data import DGData
+from tgm.graph import DGraph
 from tgm.hooks import DeduplicationHook
 
 
 @pytest.fixture
-def data():
+def dg():
     edge_index = torch.LongTensor([[2, 2], [2, 4], [1, 8]])
     edge_timestamps = torch.LongTensor([1, 5, 20])
-    return DGData.from_raw(edge_timestamps, edge_index)
+    data = DGData.from_raw(edge_timestamps, edge_index)
+    return DGraph(data)
 
 
 def test_hook_dependancies():
@@ -24,16 +26,57 @@ def test_hook_dependancies():
     }
 
 
-def test_dedup():
-    pass
+def test_dedup(dg):
+    hook = DeduplicationHook()
+    batch = dg.materialize()
+    processed_batch = hook(dg, batch)
+    torch.testing.assert_close(
+        processed_batch.unique_nids, torch.LongTensor([1, 2, 4, 8])
+    )
+    torch.testing.assert_close(
+        processed_batch.nid_to_idx, torch.LongTensor([-1, 0, 1, -1, 2, -1, -1, -1, 3])
+    )
+    torch.testing.assert_close(processed_batch.src_idx, torch.LongTensor([1, 1, 0]))
+    torch.testing.assert_close(processed_batch.dst_idx, torch.LongTensor([1, 2, 3]))
 
 
-def test_dedup_with_negatives():
-    # dg = DGraph(data)
-    # hook = NegativeEdgeSamplerHook(low=0, high=10)
-    # batch = hook(dg, dg.materialize())
-    pass
+def test_dedup_with_negatives(dg):
+    hook = DeduplicationHook()
+    batch = dg.materialize()
+    batch.neg = torch.LongTensor([1, 5, 10])  # add some mock negatives
+
+    processed_batch = hook(dg, batch)
+    torch.testing.assert_close(
+        processed_batch.unique_nids, torch.LongTensor([1, 2, 4, 5, 8, 10])
+    )
+    torch.testing.assert_close(
+        processed_batch.nid_to_idx,
+        torch.LongTensor([-1, 0, 1, -1, 2, 3, -1, -1, 4, -1, 5]),
+    )
+    torch.testing.assert_close(processed_batch.src_idx, torch.LongTensor([1, 1, 0]))
+    torch.testing.assert_close(processed_batch.dst_idx, torch.LongTensor([1, 2, 4]))
+    torch.testing.assert_close(processed_batch.neg_idx, torch.LongTensor([0, 3, 5]))
 
 
-def test_dedup_with_nbrs():
-    pass
+def test_dedup_with_nbrs(dg):
+    hook = DeduplicationHook()
+    batch = dg.materialize()
+    batch.nbr_nids = [  # add some mock neighbours
+        torch.LongTensor([1, 5]),  # First hop
+        torch.LongTensor([10]),  # Second hop
+    ]
+
+    processed_batch = hook(dg, batch)
+    torch.testing.assert_close(
+        processed_batch.unique_nids, torch.LongTensor([1, 2, 4, 5, 8, 10])
+    )
+    torch.testing.assert_close(
+        processed_batch.nid_to_idx,
+        torch.LongTensor([-1, 0, 1, -1, 2, 3, -1, -1, 4, -1, 5]),
+    )
+    torch.testing.assert_close(processed_batch.src_idx, torch.LongTensor([1, 1, 0]))
+    torch.testing.assert_close(processed_batch.dst_idx, torch.LongTensor([1, 2, 4]))
+    torch.testing.assert_close(
+        processed_batch.nbr_nids_idx[0], torch.LongTensor([0, 3])
+    )
+    torch.testing.assert_close(processed_batch.nbr_nids_idx[1], torch.LongTensor([5]))
