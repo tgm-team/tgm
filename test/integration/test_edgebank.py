46608066e795a0f61d27f8bd3ca8a834473d595d
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-06-23 18:28:53 -0400

    upd

--- test/integration/test_edgebank.py
+++ test/integration/test_edgebank.py
@@ -1,28 +1,23 @@
 import pytest
 
 
-@pytest.mark.skip('TODO')
 @pytest.mark.slow
 @pytest.mark.parametrize('dataset', ['tgbl-wiki'])
 @pytest.mark.slurm(
     resources=[
         '--partition=main',
         '--cpus-per-task=2',
-        '--mem=8G',
+        '--mem=4G',
         '--time=0:10:00',
-        '--gres=gpu:a100l:1',
     ]
 )
-def test_tgat_recency_sampler(slurm_job_runner, dataset):
+def test_edgebank(slurm_job_runner, dataset):
     cmd = f"""
 echo "Downloading dataset: {dataset}"
 echo "y" | python -c "from tgb.linkproppred.dataset import LinkPropPredDataset; LinkPropPredDataset('{dataset}')"
 
-python "$ROOT_DIR/examples/linkproppred/tgat.py" \
-    --dataset {dataset} \
-    --device cuda \
-    --epochs 1 \
-    --sampling recency"""
+python "$ROOT_DIR/examples/linkproppred/edgebank.py" \
+    --dataset {dataset}"""
     state, _ = slurm_job_runner(cmd)
 
     # TODO: Get perf and latency as artifact to upload in CI
