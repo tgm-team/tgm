713b25c19b967e64c6217ba0a9326b956198e196
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-06-24 12:49:25 -0400

    WIP

--- examples/linkproppred/tgat.py
+++ examples/linkproppred/tgat.py
@@ -116,9 +116,6 @@ class TGAT(nn.Module):
             )
             z[batch.nid_to_idx[batch.nids[hop]]] = out
 
-            # TODO: Merge layers to combine attention results and node original features
-            # z = self.merge_layers[hop](out, node_feat)
-
         z_src, z_dst, z_neg = z[batch.src_idx], z[batch.dst_idx], z[batch.neg_idx]  # type: ignore
         pos_out = self.link_predictor(z_src, z_dst)
         neg_out = self.link_predictor(z_src, z_neg)
