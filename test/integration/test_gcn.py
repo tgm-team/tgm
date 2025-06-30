852677820fe341c9c0be24bc3f48981bfcb76ec2
Jacob Chmura <jacobpaul.chmura@gmail.com>
2025-06-30 10:15:13 -0400

    Add More Integration tests (#49)
    * Upload coverage
    
    * Nameing
    
    * Codecov
    
    * Trigger ci
    
    * Restructure test files
    
    * WIP integration workflow
    
    * WIP
    
    * Mark integration tests as slow
    
    * WIP
    
    * Simplify
    
    * Update
    
    * WIP
    
    * Fix timeout
    
    * WIP
    
    * Restrict user
    
    * Export github workdir
    
    * Update root
    
    * Module load
    
    * Sync the examples dependancies
    
    * Try auto-download
    
    * typo
    
    * Try to consolidate artifacts
    
    * Change path
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Safe shell expand
    
    * WIP
    
    * Update path
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * upd
    
    * upd
    
    * setup shared context
    
    * simplify
    
    * Simplify
    
    * try slurmtmpdir
    
    * Add integration tests
    
    * Upd
    
    * Switch to tgn-trade
    
    * Merge main

--- test/integration/test_gcn.py
+++ test/integration/test_gcn.py
@@ -8,36 +8,44 @@ import pytest
         '--partition=main',
         '--cpus-per-task=2',
         '--mem=4G',
-        '--time=0:10:00',
+        '--time=3:00:00',
+        '--gres=gpu:a100l:1',
     ]
 )
-def test_edgebank_linkprop_pred(slurm_job_runner, dataset):
+def test_gcn_linkprop_pred(slurm_job_runner, dataset):
     cmd = f"""
 echo "Downloading dataset: {dataset}"
 echo "y" | python -c "from tgb.linkproppred.dataset import LinkPropPredDataset; LinkPropPredDataset('{dataset}')"
 
-python "$ROOT_DIR/examples/linkproppred/edgebank.py" \
-    --dataset {dataset}"""
+python "$ROOT_DIR/examples/linkproppred/gcn.py" \
+    --dataset {dataset} \
+    --device cuda \
+    --epochs 1
+    """
     state = slurm_job_runner(cmd)
     assert state == 'COMPLETED'
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('dataset', ['tgbl-wiki'])
+@pytest.mark.parametrize('dataset', ['tgbn-trade'])
 @pytest.mark.slurm(
     resources=[
         '--partition=main',
         '--cpus-per-task=2',
         '--mem=4G',
-        '--time=0:10:00',
+        '--time=3:00:00',
+        '--gres=gpu:a100l:1',
     ]
 )
-def test_edgebank_tgb_linkprop_pred(slurm_job_runner, dataset):
+def test_gcn_nodeprop_pred(slurm_job_runner, dataset):
     cmd = f"""
 echo "Downloading dataset: {dataset}"
-echo "y" | python -c "from tgb.linkproppred.dataset import LinkPropPredDataset; LinkPropPredDataset('{dataset}')"
+echo "y" | python -c "from tgb.nodeproppred.dataset import NodePropPredDataset; NodePropPredDataset('{dataset}')"
 
-python "$ROOT_DIR/examples/linkproppred/TGB/edgebank.py" \
-    --dataset {dataset}"""
+python "$ROOT_DIR/examples/nodeproppred/gcn.py" \
+    --dataset {dataset} \
+    --device cuda \
+    --epochs 1
+    """
     state = slurm_job_runner(cmd)
     assert state == 'COMPLETED'
