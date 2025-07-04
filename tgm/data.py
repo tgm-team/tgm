c1338da2ab3e4bee9c9653ef6babe6674a53c14c
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-07-04 06:44:34 -0400

    Better err msgs in data

--- tgm/data.py
+++ tgm/data.py
@@ -32,9 +32,11 @@ class DGData:
     def __post_init__(self) -> None:
         # Validate edge index
         if not isinstance(self.edge_index, Tensor):
-            raise TypeError('edge_index must be a Tensor')
+            raise TypeError(f'edge_index must be Tensor, got: {type(self.edge_index)}')
         if self.edge_index.ndim != 2 or self.edge_index.shape[1] != 2:
-            raise ValueError('edge_index must have shape [num_edges, 2]')
+            raise ValueError(
+                f'edge_index must have shape [num_edges, 2], got: {self.edge_index.shape}',
+            )
 
         num_edges = self.edge_index.shape[0]
         if num_edges == 0:
@@ -42,42 +44,64 @@ class DGData:
 
         # Validate edge event idx
         if not isinstance(self.edge_event_idx, Tensor):
-            raise TypeError('edge_event_idx must be a Tensor')
+            raise TypeError(
+                f'edge_event_idx must be Tensor, got: {type(self.edge_event_idx)}'
+            )
         if self.edge_event_idx.ndim != 1 or self.edge_event_idx.shape[0] != num_edges:
-            raise ValueError('edge_event_idx must have shape [num_edges]')
+            raise ValueError(
+                'edge_event_idx must have shape [num_edges], '
+                f'got {num_edges} edges and shape {self.edge_event_idx.shape}'
+            )
 
         # Validate edge features
         if self.edge_feats is not None:
             if not isinstance(self.edge_feats, Tensor):
-                raise TypeError('edge_feats must be a Tensor')
+                raise TypeError(
+                    f'edge_feats must be Tensor, got: {type(self.edge_feats)}'
+                )
             if self.edge_feats.ndim != 2 or self.edge_feats.shape[0] != num_edges:
-                raise ValueError('edge_feats must have shape [num_edges, D_edge]')
+                raise ValueError(
+                    'edge_feats must have shape [num_edges, D_edge], '
+                    f'got {num_edges} edges and shape {self.edge_feats.shape}'
+                )
 
         # Validate node event idx
         num_node_events = 0
         if self.node_event_idx is not None:
             if not isinstance(self.node_event_idx, Tensor):
-                raise TypeError('node_event_idx must be a Tensor')
+                raise TypeError(
+                    f'node_event_idx must be Tensor, got: {type(self.node_event_idx)}'
+                )
             if self.node_event_idx.ndim != 1:
-                raise ValueError('node_event_idx must have shape [num_node_events]')
+                raise ValueError(
+                    f'node_event_idx must have shape [num_node_events], got: {self.node_event_idx.shape}'
+                )
             num_node_events = self.node_event_idx.shape[0]
 
             # Validate node ids
             if not isinstance(self.node_ids, Tensor):
-                raise TypeError('node_ids must be a Tensor')
+                raise TypeError(
+                    f'node_ids must be a Tensor, got: {type(self.node_ids)}'
+                )
             if self.node_ids.ndim != 1 or self.node_ids.shape[0] != num_node_events:
-                raise ValueError('node_ids must have shape [num_node_events]')
+                raise ValueError(
+                    'node_ids must have shape [num_node_events], ',
+                    f'got {num_node_events} node events and shape {self.node_ids.shape}',
+                )
 
             # Validate dynamic node features (could be None)
             if self.dynamic_node_feats is not None:
                 if not isinstance(self.dynamic_node_feats, Tensor):
-                    raise TypeError('dynamic_node_feats must be a Tensor')
+                    raise TypeError(
+                        f'dynamic_node_feats must be a Tensor, got: {type(self.dynamic_node_feats)}'
+                    )
                 if (
                     self.dynamic_node_feats.ndim != 2
                     or self.dynamic_node_feats.shape[0] != num_node_events
                 ):
                     raise ValueError(
-                        'dynamic_node_feats must have shape [num_node_events, D_node_dynamic]'
+                        'dynamic_node_feats must have shape [num_node_events, D_node_dynamic], '
+                        f'got {num_node_events} node events and shape {self.dynamic_node_feats.shape}'
                     )
         else:
             if self.node_ids is not None:
@@ -94,25 +118,33 @@ class DGData:
 
         if self.static_node_feats is not None:
             if not isinstance(self.static_node_feats, Tensor):
-                raise TypeError('static_node_feats must be a Tensor')
+                raise TypeError(
+                    f'static_node_feats must be a Tensor, got: {type(self.static_node_feats)}'
+                )
             if (
                 self.static_node_feats.ndim != 2
                 or self.static_node_feats.shape[0] != num_nodes
             ):
                 raise ValueError(
-                    'static_node_feats must have shape [num_nodes, D_node_static]'
+                    'static_node_feats must have shape [num_nodes, D_node_static], '
+                    f'got {num_nodes} nodes and shape {self.static_node_feats.shape}'
                 )
 
         # Validate timestamps
         if not isinstance(self.timestamps, Tensor):
-            raise TypeError('timestamps must be a Tensor')
+            raise TypeError(
+                f'timestamps must be a Tensor, got: {type(self.timestamps)}'
+            )
         if (
             self.timestamps.ndim != 1
             or self.timestamps.shape[0] != num_edges + num_node_events
         ):
-            raise ValueError('timestamps must have shape [num_edges + num_node_events]')
+            raise ValueError(
+                'timestamps must have shape [num_edges + num_node_events], '
+                f'got {num_edges} edges, {num_node_events} node_events, shape: {self.timestamps.shape} '
+            )
         if not torch.all(self.timestamps >= 0):
-            raise ValueError('timestamps must be non-negative integers')
+            raise ValueError('timestamps must all be non-negative')
 
         # Sort if necessary
         if not torch.all(torch.diff(self.timestamps) >= 0):
