37e9812e80f99e6c8800680636830ee5671af30e
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-07-10 08:39:05 -0400

    Clean up example

--- examples/nodeproppred/persistant_forecast.py
+++ examples/nodeproppred/persistant_forecast.py
@@ -13,68 +13,6 @@ from tgm import DGraph
 from tgm.loader import DGDataLoader
 from tgm.util.seed import seed_everything
 
-
-class PersistantForecaster:
-    def __init__(self, num_class):
-        self.dict = {}
-        self.num_class = num_class
-
-    def update_dict(self, node_id, label):
-        self.dict[node_id] = label
-
-    def query_dict(self, node_id):
-        r"""Query the dictionary for the last seen label of a node.
-
-        Args:
-            node_id: the node to query
-        Returns:
-            returns the last seen label of the node if it exists, if not return zero vector
-        """
-        if node_id in self.dict:
-            return self.dict[node_id]
-        else:
-            return np.zeros(self.num_class)
-
-
-def test_n_upate(
-    loader: DGDataLoader,
-    forecaster: PersistantForecaster,
-) -> dict:
-    eval_metric = 'ndcg'
-    total_score = 0
-    num_labels = 0
-    num_label_batches = 0
-    for batch in tqdm(loader):
-        if batch.dynamic_node_feats is None:
-            continue
-
-        preds = []
-        label = batch.dynamic_node_feats.cpu().detach().numpy()
-        label_srcs = batch.node_ids.cpu().detach().numpy()
-        for i in range(0, label_srcs.shape[0]):
-            node_id = label_srcs[i]
-            pred_vec = forecaster.query_dict(node_id)
-            preds.append(pred_vec)
-            forecaster.update_dict(node_id, label[i])
-            num_labels += 1
-
-        np_pred = np.stack(preds, axis=0)
-        np_true = label
-
-        input_dict = {
-            'y_true': np_true,
-            'y_pred': np_pred,
-            'eval_metric': [eval_metric],
-        }
-        result_dict = evaluator.eval(input_dict)
-        score = result_dict[eval_metric]
-        total_score += score
-        num_label_batches += 1
-    metric_dict = {}
-    metric_dict[eval_metric] = float(total_score) / num_label_batches
-    return metric_dict, num_labels
-
-
 parser = argparse.ArgumentParser(
     description='Persistant Forecast Example',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -95,6 +33,50 @@ parser.add_argument(
 )
 
 
+class PersistantForecaster:
+    def __init__(self, num_classes: int) -> None:
+        self.dict = {}
+        self.num_classes = num_classes
+
+    def update(self, node_id: int, label: np.ndarray) -> None:
+        self.dict[node_id] = label
+
+    def __call__(self, node_id: int) -> np.ndarray:
+        if node_id in self.dict:
+            return self.dict[node_id]
+        else:
+            return np.zeros(self.num_classes)
+
+
+def eval(loader: DGDataLoader, model: PersistantForecaster) -> dict:
+    eval_metric = 'ndcg'
+    total_score, num_batches = 0, 0
+
+    for batch in tqdm(loader):
+        if batch.dynamic_node_feats is None:
+            continue
+
+        labels = batch.dynamic_node_feats.cpu().detach().numpy()
+        node_ids = batch.node_ids.cpu().detach().numpy()
+
+        preds = []
+        for node_id, label in zip(node_ids, labels):
+            preds.append(model(node_id))
+            model.update(node_id, label)
+
+        result = evaluator.eval(
+            {
+                'y_true': labels,
+                'y_pred': np.stack(preds),
+                'eval_metric': [eval_metric],
+            }
+        )
+        total_score += result[eval_metric]
+        num_batches += 1
+
+    return {eval_metric: total_score / num_batches}
+
+
 args = parser.parse_args()
 seed_everything(args.seed)
 
@@ -102,35 +84,18 @@ train_dg = DGraph(args.dataset, time_delta=args.time_gran, split='train')
 val_dg = DGraph(args.dataset, time_delta=args.time_gran, split='val')
 test_dg = DGraph(args.dataset, time_delta=args.time_gran, split='test')
 
+train_loader = DGDataLoader(train_dg, batch_unit=args.batch_time_gran)
+val_loader = DGDataLoader(val_dg, batch_unit=args.batch_time_gran)
+test_loader = DGDataLoader(test_dg, batch_unit=args.batch_time_gran)
 
-train_loader = DGDataLoader(
-    train_dg,
-    batch_unit=args.batch_time_gran,
-)
-val_loader = DGDataLoader(
-    val_dg,
-    batch_unit=args.batch_time_gran,
-)
-test_loader = DGDataLoader(
-    test_dg,
-    batch_unit=args.batch_time_gran,
-)
-
-num_nodes = test_dg.num_nodes
-num_classes = train_dg.dynamic_node_feats_dim
 evaluator = Evaluator(name=args.dataset)
-forecaster = PersistantForecaster(num_classes)
+model = PersistantForecaster(num_classes=test_dg.dynamic_node_feats_dim)
 
-train_results, num_train_labels_tgm = test_n_upate(train_loader, forecaster)
-print('Training results:')
-print(' '.join(f'{k}={v:.4f}' for k, v in train_results.items()))
+train_results = eval(train_loader, model)
+print('Training results: ', ' '.join(f'{k}={v:.4f}' for k, v in train_results.items()))
 
+val_results = eval(val_loader, model)
+print('Validation results: ', ' '.join(f'{k}={v:.4f}' for k, v in val_results.items()))
 
-val_results, num_val_labels_tgm = test_n_upate(val_loader, forecaster)
-print('Validation results:')
-print(' '.join(f'{k}={v:.4f}' for k, v in val_results.items()))
-
-
-test_results, num_test_labels_tgm = test_n_upate(test_loader, forecaster)
-print('Test results:')
-print(' '.join(f'{k}={v:.4f}' for k, v in test_results.items()))
+test_results = eval(test_loader, model)
+print('Test results: ', ' '.join(f'{k}={v:.4f}' for k, v in test_results.items()))
