de980387ec2f9e802150bf2373d1b28f9e8f3080
Andy Huang <shenyang.huang@mail.mcgill.ca>
2025-07-11 23:00:43 +0100

    adding CI test and clean up

--- test/integration/test_gcn.py
+++ test/integration/test_gcn.py
@@ -27,6 +27,33 @@ python "$ROOT_DIR/examples/linkproppred/gcn.py" \
 
 
 @pytest.mark.integration
+@pytest.mark.parametrize('dataset', ['tgbl-wiki'])
+@pytest.mark.slurm(
+    resources=[
+        '--partition=main',
+        '--cpus-per-task=2',
+        '--mem=8G',
+        '--time=3:00:00',
+        '--gres=gpu:a100l:1',
+    ]
+)
+def test_gcn_tgb_linkprop_pred(slurm_job_runner, dataset):
+    cmd = f"""
+echo "Downloading dataset: {dataset}"
+echo "y" | python -c "from tgb.linkproppred.dataset import LinkPropPredDataset; LinkPropPredDataset('{dataset}')"
+
+python "$ROOT_DIR/examples/linkproppred/TGB/gcn.py" \
+    --dataset {dataset} \
+    --time-gran s \
+    --batch-time-gran h \
+    --device cuda \
+    --epochs 1
+    """
+    state = slurm_job_runner(cmd)
+    assert state == 'COMPLETED'
+
+
+@pytest.mark.integration
 @pytest.mark.parametrize('dataset', ['tgbn-trade'])
 @pytest.mark.slurm(
     resources=[
