79954bbdac3822f3b10419cb135123bf19202bae
Bảo Ngô <ngot1@myumanitoba.ca>
2025-07-12 19:48:10 -0500

    Init commit for DyGFormer implementation
    Created nn/model/dygformer.py.Added skeletons for NeighborCooccurrenceEncoder, TransformerEncoder, DyGFormer.

---
+++ tgm/nn/model/dygformer.py
@@ -0,0 +1,130 @@
+"""Adapted from https://github.com/yule-BUAA/DyGLib_TGB."""
+
+from typing import Callable
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from ..time_encoding import Time2Vec
+
+
+class NeighborCooccurrenceEncoder(nn.Module):
+    r"""An implementation of Neighbor Co-occurrence Encoding Scheme.
+
+    Args:
+        feat_dim (int): dimension of neighbor co-occurrence features (encodings).
+
+    Reference: https://arxiv.org/abs/2303.13047.
+    """
+
+    def __init__(
+        self,
+        feat_dim: int,
+    ) -> None:
+        super(NeighborCooccurrenceEncoder, self).__init__()
+        # @TODO
+        raise Exception('Not implemented yet')
+
+    def forward(
+        self, src_neighbour_nodes_ids: np.ndarray, dst_neighbour_nodes_ids: np.ndarray
+    ) -> torch.Tensor:
+        r"""Forward pass. Encode neighbor co-occurrence (Section 4.1).
+
+        Args:
+            src_neighbour_nodes_ids (Numpy array): Padded list of source node's neighbour.
+            dst_neighbour_nodes_ids (Numpy array): Padded list of destination node's neighbour.
+
+        Returns:
+            X (PyTorch Float Tensor): Neighbor co-occurrence features (`X^{t}_{*,C}`).
+        """
+        # @TODO (assert dimension of two inputs. Expect to be the same)
+        raise Exception('Not implemented yet')
+
+
+class TransformerEncoder(nn.Module):
+    r"""An implementation of Transformer Encoder.
+
+    Args:
+        attention_dim (int): dimension of the attention vector.
+        num_heads (int): number of attention heads.
+        dropout (float): dropout rate.
+
+    Reference: https://arxiv.org/abs/2303.13047.
+    """
+
+    def __init__(
+        self, attention_dim: int, num_heads: int, dropout: float = 0.1
+    ) -> None:
+        super(TransformerEncoder, self).__init__()
+        # @TODO
+        raise Exception('Not implemented yet')
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        r"""Forward pass. Encode the inputs by Transformer encoder (Section 4.1).
+
+        Args:
+            inputs (PyTorch Float Tensor): `Z^{t} = [Z^{t}_u, Z^{t}_v]`.
+
+        Returns:
+            H (PyTorch Float Tensor): Representations of all nodes.
+        """
+        # @TODO
+        raise Exception('Not implemented yet')
+
+
+class DyGFormer(nn.Module):
+    r"""An implementation of DyGFormer.
+
+    Args:
+        node_feat_dim (int): Dimension of static/dynamic node features (`d_N`).
+        edge_feat_dim (int): Dimension of edge features (`d_E`).
+        time_feat_dim (int): Dimension of time encodings (`d_T`).
+        channel_embedding_dim (int): Dimension of each channel embedding.
+        output_dim (int): Dimension of output embedding.
+        patch_size (int): Path size (`\mathbf{P}`).
+        num_layers (int): Number of transformer layers.
+        num_heads (int): Number of attention heads.
+        dropout (float): Drop out rate.
+        max_input_sequence_length (int): maximal length of the input sequence for each node.
+        time_encoder (PyTorch Module) : Time encoder module.
+
+    Reference: https://arxiv.org/abs/2303.13047.
+    """
+
+    def __init__(
+        self,
+        node_feat_dim: int,
+        edge_feat_dim: int,
+        time_feat_dim: int,
+        channel_embedding_dim: int,
+        output_dim: int = 172,
+        patch_size: int = 1,
+        num_layers: int = 2,
+        num_heads: int = 2,
+        dropout: float = 0.1,
+        max_input_sequence_length: int = 512,
+        time_encoder: Callable[..., nn.Module] = Time2Vec,
+    ) -> None:
+        super(DyGFormer, self).__init__()
+        # @TODO
+        raise Exception('Not implemented yet')
+
+    def forward(
+        self,
+        X: torch.Tensor,
+        edge_index: torch.Tensor,
+        edge_feat: torch.Tensor,
+    ) -> torch.Tensor:
+        f"""Forward pass.
+
+        Args:
+            X (PyTorch Float Tensor): Node features.
+            edge_index (PyTorch Long Tensor): Graph edge indices.
+            edge_feat (PyTorch Long Tensor): Edge feature vector.
+
+        Returns:
+            H (PyTorch Float Tensor): Time-aware representations of all nodes.
+        """
+        # @TODO
+        raise Exception('Not implemented yet')
