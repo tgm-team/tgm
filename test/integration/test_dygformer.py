531185802ba4d22ddb38431a111511121bab40cb
Bao Ngo <ngot1@gallium-01.cs.umanitoba.ca>
2025-08-07 20:01:43 -0500

    refractor dygformer example and add integration test

---
+++ test/integration/test_dygformer.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('dataset', ['tgbl-wiki'])
+@pytest.mark.slurm(
+    resources=[
+        '--partition=main',
+        '--cpus-per-task=2',
+        '--mem=8G',
+        '--time=3:00:00',
+        '--gres=gpu:a100l:1',
+    ]
+)
+def test_tgat_recency_sampler_linkprop_pred(slurm_job_runner, dataset):
+    cmd = f"""
+echo "Downloading dataset: {dataset}"
+echo "y" | python -c "from tgb.linkproppred.dataset import LinkPropPredDataset; LinkPropPredDataset('{dataset}')"
+
+python "$ROOT_DIR/examples/linkproppred/dygformer.py" \
+    --dataset {dataset} \
+    --device cuda \
+    --epochs 1 \
+    """
+    state = slurm_job_runner(cmd)
+    assert state == 'COMPLETED'
