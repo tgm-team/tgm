ecaac6d19513d7f1062425c68eac9169e002580d
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-08-14 20:50:07 -0400

    device

--- examples/nodeproppred/persistant_forecast.py
+++ examples/nodeproppred/persistant_forecast.py
@@ -80,15 +80,9 @@ def eval(loader: DGDataLoader, model: PersistantForecaster) -> dict:
 args = parser.parse_args()
 seed_everything(args.seed)
 
-train_dg = DGraph(args.dataset, split='train', device=args.device).discretize(
-    args.time_gran
-)
-val_dg = DGraph(args.dataset, split='val', device=args.device).discretize(
-    args.time_gran
-)
-test_dg = DGraph(args.dataset, split='val', device=args.device).discretize(
-    args.time_gran
-)
+train_dg = DGraph(args.dataset, split='train').discretize(args.time_gran)
+val_dg = DGraph(args.dataset, split='val').discretize(args.time_gran)
+test_dg = DGraph(args.dataset, split='test').discretize(args.time_gran)
 
 train_loader = DGDataLoader(train_dg, batch_unit=args.batch_time_gran)
 val_loader = DGDataLoader(val_dg, batch_unit=args.batch_time_gran)
