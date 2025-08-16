4a5eb0e69dbc4199670e25c95cf2707aa62b564c
Bảo Ngô <ngot1@myumanitoba.ca>
2025-08-16 16:01:23 -0500

    Added PersistantForecast for GraphPropPred example

---
+++ examples/graphproppred/persistant_forecast.py
@@ -0,0 +1,168 @@
+import argparse
+from typing import Callable
+
+import pandas as pd
+import torch
+from torchmetrics import Metric, MetricCollection
+from torchmetrics.classification import BinaryAUROC, BinaryAveragePrecision
+from tqdm import tqdm
+
+from tgm import DGBatch, DGraph
+from tgm.loader import DGDataLoader
+
+parser = argparse.ArgumentParser(
+    description='TGCN Graph Property Example',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument('--seed', type=int, default=1337, help='random seed to use')
+parser.add_argument(
+    '--path-dataset',
+    type=str,
+    default='examples/graphproppred/test_token.csv',
+    help='Path to dataset csv file',
+)
+
+parser.add_argument(
+    '--raw-time-gran',
+    type=str,
+    default='s',
+    help='raw time granularity for dataset',
+)
+parser.add_argument(
+    '--batch-time-gran',
+    type=str,
+    default='W',
+    help='time granularity to operate on for snapshots',
+)
+
+
+def edge_count(snapshot: DGBatch):
+    # return number of edges of current snapshot
+    return snapshot.src.shape[0]
+
+
+def node_count(snapshot: DGBatch):
+    # return number of ndoes of current snapshot
+    return torch.unique(torch.cat([snapshot.src, snapshot.dst])).numel()
+
+
+def label_generator_next_binary_classification(
+    loader: DGDataLoader, snapshot_measurement: Callable = node_count
+) -> torch.Tensor:
+    labels = []
+    prev_snapshot_metric = -1
+    for idx, snapshot in enumerate(loader):
+        if idx == 0:
+            prev_snapshot_metric = snapshot_measurement(snapshot)
+        else:
+            curr_snapshot_metric = snapshot_measurement(snapshot)
+            label = 1 if curr_snapshot_metric > prev_snapshot_metric else 0
+            labels.append(label)
+            prev_snapshot_metric = curr_snapshot_metric
+    return torch.tensor(labels, dtype=torch.int64)
+
+
+# ETL step
+def preproccess_raw_data(dataframe: pd.DataFrame) -> pd.DataFrame:
+    # time offset
+    start_time = dataframe['timestamp'].min()
+    dataframe['timestamp'] = dataframe['timestamp'].apply(lambda x: x - start_time)
+
+    # normalize edge weight
+    max_weight = float(dataframe['value'].max())
+    min_weight = float(dataframe['value'].min())
+    dataframe['value'] = dataframe['value'].apply(
+        lambda x: 1 + (9 * ((float(x) - min_weight) / (max_weight - min_weight)))
+    )
+
+    # Key generator
+    node_id_map = {}
+    dataframe['from'] = dataframe['from'].apply(
+        lambda x: node_id_map.setdefault(x, len(node_id_map))
+    )
+    dataframe['to'] = dataframe['to'].apply(
+        lambda x: node_id_map.setdefault(x, len(node_id_map))
+    )
+
+    dataframe['value'] = dataframe['value'].apply(lambda x: [x])
+
+    return dataframe
+
+
+class PersistantForecast:
+    def __init__(self, task: Callable = node_count):
+        self.comparator = task
+        self.prev_measurement = None
+
+    def reset(self) -> None:
+        self.prev_measurement = None
+
+    def __call__(self, batch: DGBatch) -> torch.tensor:
+        current_measurement = self.comparator(batch)
+
+        if self.prev_measurement is None:
+            pred = torch.randint(0, 2, ())  # random guess for the first prediction
+        else:
+            pred = (
+                torch.tensor(1)
+                if current_measurement > self.prev_measurement
+                else torch.tensor(0)
+            )
+        self.prev_measurement = current_measurement
+
+        return pred
+
+
+def eval(
+    loader: DGDataLoader,
+    y_true: torch.Tensor,
+    model: PersistantForecast,
+    metrics: Metric,
+    ignore_last_snapshot=False,
+) -> dict:
+    all_preds = []
+    number_of_snapshot = len(loader)
+    for idx, snapshot in tqdm(enumerate(loader)):
+        if not (ignore_last_snapshot and idx == number_of_snapshot - 1):
+            pred = model(snapshot)
+            all_preds.append(pred)
+    y_pred = torch.Tensor(all_preds).float()
+    indexes = torch.zeros(y_pred.size(0), dtype=torch.long, device=y_pred.device)
+
+    # TODO: For debug purpose only. will be removed before merge to main
+    assert torch.all(y_pred[1:] == y_true[: y_true.shape[0] - 1])
+    metrics(y_pred, y_true, indexes=indexes)
+    return metrics.compute()
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    metrics = [BinaryAveragePrecision(), BinaryAUROC()]
+    val_metrics = MetricCollection(metrics, prefix='Validation')
+
+    token = pd.read_csv(args.path_dataset)
+    token = preproccess_raw_data(token)
+
+    dgraph_token = DGraph.from_pandas(  # @TODO: adapt changes in discretize APIs
+        edge_df=token,
+        edge_src_col='from',
+        edge_dst_col='to',
+        edge_time_col='timestamp',
+        edge_feats_col='value',
+        time_delta=args.raw_time_gran,
+        device='cpu',
+    )
+    # TODO: Get split from new APIs
+    dgraph_token = dgraph_token.discretize(args.batch_time_gran)
+
+    full_loader = DGDataLoader(
+        dgraph_token,
+        batch_unit=args.batch_time_gran,
+    )
+    labels = label_generator_next_binary_classification(
+        full_loader
+    )  # shape: number of snapshots - 1
+
+    baseline = PersistantForecast(node_count)
+    results = eval(full_loader, labels, baseline, val_metrics, True)
+    print(results)
