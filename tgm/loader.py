af0e355a5b5b5a1427412b7df9f44eea4445d211
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-08-17 13:06:24 -0400

    SkippableDataLoaderMixin

--- tgm/loader.py
+++ tgm/loader.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Any, List
+from abc import ABC, abstractmethod
+from typing import Any, Iterator, List, Literal
 
 import torch
 
@@ -9,19 +10,52 @@ from tgm.hooks import DGHook, HookManager
 from tgm.timedelta import TimeDeltaDG
 
 
-class DGDataLoader(torch.utils.data.DataLoader):
+class _SkippableDataLoaderMixin(ABC):
+    r"""Mixin to optionally skip or raise on empty batches."""
+
+    def __init__(
+        self,
+        *args: Any,
+        on_empty: Literal['skip', 'raise', None] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        valid_on_empty = ['skip', 'raise', None]
+        if on_empty not in valid_on_empty:
+            raise ValueError(
+                f'Invalid on_empty={on_empty}, expected one of: {valid_on_empty}'
+            )
+        self._on_empty = on_empty
+
+    @abstractmethod
+    def is_batch_empty(self, batch: Any) -> bool: ...
+
+    def __iter__(self) -> Iterator[Any]:
+        for batch in super().__iter__():  # type: ignore
+            if self.is_batch_empty(batch):
+                if self._on_empty == 'raise':
+                    raise ValueError('Empty batch encountered')
+                elif self._on_empty == 'skip':
+                    continue
+            yield batch
+
+
+class DGDataLoader(_SkippableDataLoaderMixin, torch.utils.data.DataLoader):  # type: ignore
     r"""Iterate and materialize from a DGraph.
 
     Args:
         dg (DGraph): The dynamic graph to iterate.
         batch_size (int): The batch size to yield at each iteration.
         batch_unit (str): The unit corresponding to the batch_size.
+        on_empty (Literal['skip', 'raise', None]): Action for empty batches.
         hook (HookManager | Hook | List[Hook] | None): Arbitrary transform behaviour to execute before materializing a batch.
         **kwargs (Any): Additional arguments to torch.utils.data.DataLoader.
 
     Raises:
         ValueError: If the batch_unit and dg time unit are not both ordered or both not ordered.
         ValueError: If the batch_unit and dg time unit are both ordered but the graph is coarser than the batch.
+        ValueError: If an empty batch was encountered an on_empty='raise'.
     """
 
     def __init__(
@@ -29,6 +63,7 @@ class DGDataLoader(torch.utils.data.DataLoader):
         dg: DGraph,
         batch_size: int = 1,
         batch_unit: str = 'r',
+        on_empty: Literal['skip', 'raise', None] = None,
         hook: HookManager | DGHook | List[DGHook] | None = None,
         **kwargs: Any,
     ) -> None:
@@ -52,8 +87,7 @@ class DGDataLoader(torch.utils.data.DataLoader):
             batch_size = int(batch_time_delta.convert(dg.time_delta))
 
         # Warning: Cache miss
-        assert dg.start_time is not None
-        assert dg.end_time is not None
+        assert dg.start_time is not None and dg.end_time is not None
 
         self._dg = dg
         self._batch_size = batch_size
@@ -67,9 +101,15 @@ class DGDataLoader(torch.utils.data.DataLoader):
             slice_start = range(start_idx, stop_idx - batch_size, batch_size)
         else:
             slice_start = range(start_idx, stop_idx, batch_size)
-        super().__init__(slice_start, 1, shuffle=False, collate_fn=self, **kwargs)  # type: ignore
+
+        super().__init__(
+            slice_start, 1, shuffle=False, collate_fn=self, on_empty=on_empty, **kwargs
+        )
 
     def __call__(self, slice_start: List[int]) -> DGBatch:
         slice_end = slice_start[0] + self._batch_size
         batch = self._slice_op(slice_start[0], slice_end)
         return self._hook(batch)
+
+    def is_batch_empty(self, batch: DGBatch) -> bool:
+        return batch.src.numel() == 0
