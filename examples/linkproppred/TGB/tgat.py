6303d44dc63c7fc161ffbb72cd6e403189c02728
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-08-21 11:58:24 -0400

    Update

--- examples/linkproppred/TGB/tgat.py
+++ examples/linkproppred/TGB/tgat.py
@@ -74,7 +74,7 @@ class MultiHeadAttention(nn.Module):
         node_feat_dim: int,
         edge_feat_dim: int,
         time_feat_dim: int,
-        num_heads: int = 2,
+        n_heads: int = 2,
         dropout: float = 0.1,
     ):
         super(MultiHeadAttention, self).__init__()
@@ -82,37 +82,37 @@ class MultiHeadAttention(nn.Module):
         self.node_feat_dim = node_feat_dim
         self.edge_feat_dim = edge_feat_dim
         self.time_feat_dim = time_feat_dim
-        self.num_heads = num_heads
+        self.n_heads = n_heads
         self.query_dim = node_feat_dim + time_feat_dim
         self.key_dim = node_feat_dim + edge_feat_dim + time_feat_dim
 
-        if self.query_dim % num_heads != 0:
+        if self.query_dim % n_heads != 0:
             print(
-                'warning: the query_dim cannot be divided by num_heads, perform padding to support the computation'
+                'warning: the query_dim cannot be divided by n_heads, perform padding to support the computation'
             )
-            self.pad_dim = num_heads - self.query_dim % num_heads
+            self.pad_dim = n_heads - self.query_dim % n_heads
             self.query_dim += self.pad_dim
         else:
             self.pad_dim = 0
 
-        assert self.query_dim % num_heads == 0, (
-            'The sum of node_feat_dim and time_feat_dim should be divided by num_heads!'
+        assert self.query_dim % n_heads == 0, (
+            'The sum of node_feat_dim and time_feat_dim should be divided by n_heads!'
         )
 
-        self.head_dim = self.query_dim // num_heads
+        self.head_dim = self.query_dim // n_heads
         self.query_projection = nn.Linear(
-            self.query_dim, num_heads * self.head_dim, bias=False
+            self.query_dim, n_heads * self.head_dim, bias=False
         )
         self.key_projection = nn.Linear(
-            self.key_dim, num_heads * self.head_dim, bias=False
+            self.key_dim, n_heads * self.head_dim, bias=False
         )
         self.value_projection = nn.Linear(
-            self.key_dim, num_heads * self.head_dim, bias=False
+            self.key_dim, n_heads * self.head_dim, bias=False
         )
 
         self.scaling_factor = self.head_dim**-0.5
         self.layer_norm = nn.LayerNorm(self.query_dim)
-        self.residual_fc = nn.Linear(num_heads * self.head_dim, self.query_dim)
+        self.residual_fc = nn.Linear(n_heads * self.head_dim, self.query_dim)
         self.dropout = nn.Dropout(dropout)
 
     def forward(
@@ -141,9 +141,9 @@ class MultiHeadAttention(nn.Module):
 
         # Tensor, shape (batch_size, 1, query_dim)
         query = residual = torch.cat([node_features, node_time_features], dim=2)
-        # shape (batch_size, 1, num_heads, self.head_dim)
+        # shape (batch_size, 1, n_heads, self.head_dim)
         query = self.query_projection(query).reshape(
-            query.shape[0], query.shape[1], self.num_heads, self.head_dim
+            query.shape[0], query.shape[1], self.n_heads, self.head_dim
         )
 
         # Tensor, shape (batch_size, num_neighbors, node_feat_dim + edge_feat_dim + time_feat_dim)
@@ -155,24 +155,24 @@ class MultiHeadAttention(nn.Module):
             ],
             dim=2,
         )
-        # Tensor, shape (batch_size, num_neighbors, num_heads, self.head_dim)
+        # Tensor, shape (batch_size, num_neighbors, n_heads, self.head_dim)
         self.key_projection(key)
         key = self.key_projection(key).reshape(
-            key.shape[0], key.shape[1], self.num_heads, self.head_dim
+            key.shape[0], key.shape[1], self.n_heads, self.head_dim
         )
-        # Tensor, shape (batch_size, num_neighbors, num_heads, self.head_dim)
+        # Tensor, shape (batch_size, num_neighbors, n_heads, self.head_dim)
         value = self.value_projection(value).reshape(
-            value.shape[0], value.shape[1], self.num_heads, self.head_dim
+            value.shape[0], value.shape[1], self.n_heads, self.head_dim
         )
 
-        # Tensor, shape (batch_size, num_heads, 1, self.head_dim)
+        # Tensor, shape (batch_size, n_heads, 1, self.head_dim)
         query = query.permute(0, 2, 1, 3)
-        # Tensor, shape (batch_size, num_heads, num_neighbors, self.head_dim)
+        # Tensor, shape (batch_size, n_heads, num_neighbors, self.head_dim)
         key = key.permute(0, 2, 1, 3)
-        # Tensor, shape (batch_size, num_heads, num_neighbors, self.head_dim)
+        # Tensor, shape (batch_size, n_heads, num_neighbors, self.head_dim)
         value = value.permute(0, 2, 1, 3)
 
-        # Tensor, shape (batch_size, num_heads, 1, num_neighbors)
+        # Tensor, shape (batch_size, n_heads, 1, num_neighbors)
         attention = torch.einsum('bhld,bhnd->bhln', query, key)
         attention = attention * self.scaling_factor
 
@@ -181,23 +181,23 @@ class MultiHeadAttention(nn.Module):
             torch.from_numpy(neighbor_masks).to(node_features.device).unsqueeze(dim=1)
         )
         attention_mask = attention_mask == 0
-        # Tensor, shape (batch_size, self.num_heads, 1, num_neighbors)
+        # Tensor, shape (batch_size, self.n_heads, 1, num_neighbors)
         attention_mask = torch.stack(
-            [attention_mask for _ in range(self.num_heads)], dim=1
+            [attention_mask for _ in range(self.n_heads)], dim=1
         )
 
-        # Tensor, shape (batch_size, self.num_heads, 1, num_neighbors)
+        # Tensor, shape (batch_size, self.n_heads, 1, num_neighbors)
         # note that if a node has no valid neighbor (whose neighbor_masks are all zero), directly set the masks to -np.inf will make the
         # attention scores after softmax be nan. Therefore, we choose a very large negative number (-1e10 following TGAT) instead of -np.inf to tackle this case
         attention = attention.masked_fill(attention_mask, -1e10)
 
-        # Tensor, shape (batch_size, num_heads, 1, num_neighbors)
+        # Tensor, shape (batch_size, n_heads, 1, num_neighbors)
         attention_scores = self.dropout(torch.softmax(attention, dim=-1))
 
-        # Tensor, shape (batch_size, num_heads, 1, self.head_dim)
+        # Tensor, shape (batch_size, n_heads, 1, self.head_dim)
         attention_output = torch.einsum('bhln,bhnd->bhld', attention_scores, value)
 
-        # Tensor, shape (batch_size, 1, num_heads * self.head_dim), where num_heads * self.head_dim is equal to query_dim
+        # Tensor, shape (batch_size, 1, n_heads * self.head_dim), where n_heads * self.head_dim is equal to query_dim
         attention_output = attention_output.permute(0, 2, 1, 3).flatten(start_dim=2)
 
         # Tensor, shape (batch_size, 1, query_dim)
@@ -208,9 +208,8 @@ class MultiHeadAttention(nn.Module):
 
         # Tensor, shape (batch_size, query_dim)
         output = output.squeeze(dim=1)
-        # Tensor, shape (batch_size, num_heads, num_neighbors)
+        # Tensor, shape (batch_size, n_heads, num_neighbors)
         attention_scores = attention_scores.squeeze(dim=2)
-
         return output, attention_scores
 
 
@@ -221,12 +220,12 @@ class TGAT(nn.Module):
         edge_raw_features: np.ndarray,
         time_dim: int,
         embed_dim: int,
-        num_layers: int = 2,
-        num_heads: int = 2,
+        num_layers: int,
+        n_heads: int = 2,
         dropout: float = 0.1,
         device: str = 'cpu',
         num_nbrs=None,
-    ):
+    ) -> None:
         super().__init__()
 
         self.num_nbrs = num_nbrs
@@ -247,12 +246,11 @@ class TGAT(nn.Module):
                     node_feat_dim=self.node_feat_dim,
                     edge_feat_dim=self.edge_feat_dim,
                     time_feat_dim=time_dim,
-                    num_heads=num_heads,
+                    n_heads=n_heads,
                     dropout=dropout,
                 )
             ]
         )
-        # follow the TGAT paper, use merge layer to combine the attention results and node original feature
         self.merge_layers = nn.ModuleList(
             [
                 MergeLayer(
@@ -271,7 +269,7 @@ class TGAT(nn.Module):
                         node_feat_dim=embed_dim,
                         edge_feat_dim=self.edge_feat_dim,
                         time_feat_dim=time_dim,
-                        num_heads=num_heads,
+                        n_heads=n_heads,
                         dropout=dropout,
                     )
                 )
@@ -311,7 +309,6 @@ class TGAT(nn.Module):
                 idx=idx,
                 inference=inference,
             )
-
         dst_node_embeddings = self.compute_node_temporal_embeddings(
             node_ids=dst_node_ids,
             node_interact_times=node_interact_times,
@@ -780,7 +777,7 @@ encoder = TGAT(
     time_dim=args.time_dim,
     embed_dim=args.embed_dim,
     num_layers=len(args.n_nbrs),
-    num_heads=args.n_heads,
+    n_heads=args.n_heads,
     dropout=float(args.dropout),
     device=args.device,
     num_nbrs=args.n_nbrs,
