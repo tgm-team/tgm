f76eaa9e3d9717e225cb2459694b8b8e656e7d08
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-08-24 15:09:46 -0400

    upd

--- tgm/hooks.py
+++ tgm/hooks.py
@@ -152,7 +152,7 @@ class DeduplicationHook(StatelessHook):
         unique_nids = torch.unique(all_nids, sorted=True)
 
         batch.unique_nids = unique_nids  # type: ignore
-        batch.global_to_local = lambda x: torch.searchsorted(batch.nids[0], x)  # type: ignore
+        batch.global_to_local = lambda x: torch.searchsorted(batch.unique_nids, x)  # type: ignore
 
         return batch
 
