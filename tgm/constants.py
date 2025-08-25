5f34b9e29bfee5f14f12574eacf48f81aa694562
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-08-25 17:50:38 -0400

    Spelling

--- tgm/constants.py
+++ tgm/constants.py
@@ -1,3 +1,3 @@
 from typing import Final
 
-INVALID_NODE_ID: Final[int] = -1  # Sentinal id marking 'no valid neighbors'
+INVALID_NODE_ID: Final[int] = -1  # Sentinel node id marking 'no valid neighbors'
