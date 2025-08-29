8a7226358726ee14d52d223a0d869c434e435f13
Bảo Ngô <ngot1@myumanitoba.ca>
2025-08-28 20:58:13 -0500

    Implemented FeedForwardNet and MLPMixer

--- tgm/nn/tpnet.py
+++ tgm/nn/tpnet.py
@@ -46,8 +46,19 @@ class FeedForwardNet(nn.Module):
         self, input_dim: int, dim_expansion_factor: float, dropout: float = 0.0
     ) -> None:
         super(FeedForwardNet, self).__init__()
-        # @TODO
-        raise Exception('Not yet implement')
+        self.ffn = nn.Sequential(
+            nn.Linear(
+                in_features=input_dim,
+                out_features=int(dim_expansion_factor * input_dim),
+            ),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(
+                in_features=int(dim_expansion_factor * input_dim),
+                out_features=input_dim,
+            ),
+            nn.Dropout(dropout),
+        )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         f"""Forward pass.
@@ -58,8 +69,7 @@ class FeedForwardNet(nn.Module):
         Returns:
             H : Output tensor
         """
-        # @TODO
-        raise Exception('Not yet implement')
+        raise self.ffn(X)
 
 
 class MLPMixer(nn.Module):
@@ -72,11 +82,24 @@ class MLPMixer(nn.Module):
         dropout: float = 0.0,
     ) -> None:
         super(MLPMixer, self).__init__()
-        # @TODO
-        raise Exception('Not yet implement')
+
+        self.token_norm = nn.LayerNorm(num_tokens)
+        self.token_feedforward = FeedForwardNet(
+            input_dim=num_tokens,
+            dim_expansion_factor=token_dim_expansion_factor,
+            dropout=dropout,
+        )
+
+        self.channel_norm = nn.LayerNorm(num_channels)
+        self.channel_feedforward = FeedForwardNet(
+            input_dim=num_channels,
+            dim_expansion_factor=channel_dim_expansion_factor,
+            dropout=dropout,
+        )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         f"""Forward pass.
+        Eq. 6 (Section  3.2)
 
         Args:
             X (PyTorch Float Tensor): Input tensor
@@ -84,8 +107,14 @@ class MLPMixer(nn.Module):
         Returns:
             H : Output tensor
         """
-        # @TODO
-        raise Exception('Not yet implement')
+        H_l_tilde = self.token_norm(X.permute(0, 2, 1))
+        H_l_tilde = self.token_feedforward(H_l_tilde).permute(0, 2, 1)
+        Z_l_tilde = X + H_l_tilde
+
+        H_l = self.channel_norm(Z_l_tilde)
+        H_l = self.channel_feedforward(H_l)
+        Z_l = Z_l_tilde + H_l
+        return Z_l
 
 
 class TPNet(nn.Module):
