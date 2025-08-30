6f35d14270bdb14f1f95b2a5433f3208008c9b1b
Bảo Ngô <ngot1@myumanitoba.ca>
2025-08-30 14:14:15 -0500

    Added draft implementation of class RandomProjectionModule for TPNet

--- tgm/nn/tpnet.py
+++ tgm/nn/tpnet.py
@@ -1,5 +1,7 @@
-from typing import Callable, Tuple
+import math
+from typing import Callable, List, Tuple
 
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -10,20 +12,81 @@ class RandomProjectionModule(nn.Module):
     def __init__(
         self,
         num_nodes: int,
-        num_layer: float,
+        num_layer: int,
         time_decay_weight: float,
         beginning_time: torch.FloatType,
         use_matrix: bool = True,
-        scale_random_projection: bool = True,  # @XXX: Use this variable with attenttion since it is opposite original implementation
+        scale_random_projection: bool = True,
         enforce_dim: int | None = None,
         num_edges: int | None = None,
         dim_factor: int | None = None,
         device: str = 'cpu',
     ) -> None:
         super(RandomProjectionModule, self).__init__()
-        assert (not num_edges and not dim_factor) or not enforce_dim
-        # @TODO
-        raise Exception('Not yet implement')
+        assert (not num_edges and not dim_factor) or not enforce_dim or use_matrix
+
+        if enforce_dim is not None:
+            self.dim = enforce_dim
+        elif num_edges is not None and dim_factor is not None:
+            self.dim = min(int(math.log(num_edges * 2)) * dim_factor, num_nodes)
+        elif use_matrix:
+            self.dim = num_nodes
+        else:
+            raise ValueError(
+                'Must provide enforce_dim or (num_edges and dim_factor) or set use_matrix to true'
+            )
+        self.num_layer = num_layer
+        self.time_decay_weight = time_decay_weight
+        self.use_matrix = use_matrix
+        self.device = device
+        self.scale = scale_random_projection
+
+        self.beginning_time = nn.Parameter(
+            torch.tensor(beginning_time), requires_grad=False
+        ).to(device)
+        self.now_time = nn.Parameter(
+            torch.tensor(beginning_time), requires_grad=False
+        ).to(device)
+        self.random_projections = nn.ParameterList()
+
+        if use_matrix:
+            for i in range(self.num_layer + 1):
+                if i == 0:
+                    self.random_projections.append(
+                        nn.Parameter(torch.eye(self.dim), requires_grad=False)
+                    )
+                else:
+                    self.random_projections.append(
+                        nn.Parameter(
+                            torch.zeros_like(self.random_projections[i - 1]),
+                            requires_grad=False,
+                        )
+                    )
+        else:
+            for i in range(self.num_layer + 1):
+                if i == 0:
+                    self.random_projections.append(
+                        nn.Parameter(
+                            torch.normal(
+                                0, 1 / math.sqrt(self.dim), (num_nodes, self.dim)
+                            ),
+                            requires_grad=False,
+                        )
+                    )
+                else:
+                    self.random_projections.append(
+                        nn.Parameter(
+                            torch.zeros_like(self.random_projections[i - 1]),
+                            requires_grad=False,
+                        )
+                    )
+
+        pair_wise_feature_dim = (2 * self.num_layer + 2) ** 2
+        self.mlp = nn.Sequential(
+            nn.Linear(pair_wise_feature_dim, pair_wise_feature_dim * 4),
+            nn.ReLU(),
+            nn.Linear(pair_wise_feature_dim * 4, pair_wise_feature_dim),
+        )
 
     def forward(self, src: torch.Tensor, dst: torch.Tensor) -> torch.Tensor:
         f"""Forward pass.
@@ -37,8 +100,130 @@ class RandomProjectionModule(nn.Module):
         Returns:
             H_pairwise : Pairwise feature
         """
-        # @TODO
-        raise Exception('Not yet implement')
+        src_random_projections = self.get_random_projections(src)
+        dst_random_projections = self.get_random_projections(dst)
+        random_projections = torch.cat(
+            [src_random_projections, dst_random_projections], dim=1
+        ).to(self.device)
+        random_feature = torch.matmul(
+            random_projections, random_projections.transpose(1, 2)
+        ).reshape(src.shape[0], -1)
+
+        if not self.scale:
+            out = self.mlp(random_feature)
+        else:
+            random_feature[random_feature < 0] = 0
+            random_feature = torch.log(random_feature + 1.0)
+            out = self.mlp(random_feature)
+        return out
+
+    def update(
+        self,
+        src: torch.Tensor,
+        dst: torch.Tensor,
+        time: torch.Tensor,
+    ) -> None:
+        f"""Updating the temporal walk matrices after observing a batch of interactions
+
+        Args:
+            src (PyTorch Tensor): Source node IDs
+            dst (PyTorch Tensor): Destination node IDs.
+            time (PyTorch Tensor): edge event time
+
+        Returns:
+        """
+        next_time = time[-1]
+        time_weight = torch.exp(-self.time_decay_weight * (next_time - time))[:, None]
+
+        for i in range(1, self.num_layer + 1):
+            self.random_projections[i].data = self.random_projections[
+                i
+            ].data * np.power(
+                np.exp(
+                    -self.time_decay_weight * (next_time - self.now_time.cpu().numpy())
+                ),
+                i,
+            )
+
+        for i in range(self.num_layer, 0, -1):
+            src_update_messages = self.random_projections[i - 1][dst] * time_weight
+            dst_update_messages = self.random_projections[i - 1][src] * time_weight
+            self.random_projections[i].scatter_add_(
+                dim=0, index=src[:, None].expand(-1, self.dim), src=src_update_messages
+            )
+            self.random_projections[i].scatter_add_(
+                dim=0, index=dst[:, None].expand(-1, self.dim), src=dst_update_messages
+            )
+
+        # set current timestamp to the biggest timestamp in this batch
+        self.now_time.data = torch.tensor(next_time, device=self.device)
+
+    def get_random_projections(self, node_ids: torch.Tensor) -> torch.Tensor:
+        f"""Get the random projections of the give node ids
+
+        Args:
+            node_ids (PyTorch Tensor): List of nodes.
+
+        Returns:
+            Random projection of nodes
+        """
+        random_projections = []
+        for i in range(self.num_layer + 1):
+            random_projections.append(self.random_projections[i][node_ids])
+        return torch.stack(random_projections, dim=1).to(self.device)
+
+    def reset_random_projections(
+        self,
+        reset_zero: bool = True,
+    ) -> None:
+        f"""Get the random projections of the give node ids
+
+        Args:
+            reset_zero (bool): whether reset temporal walk matrices to zero
+
+        Returns:
+        """
+        for i in range(1, self.num_layer + 1):
+            nn.init.zeros_(self.random_projections[i])
+        self.now_time.data = self.beginning_time.clone()
+        if not self.use_matrix and reset_zero:
+            nn.init.normal_(
+                self.random_projections[0], mean=0, std=1 / math.sqrt(self.dim)
+            )
+
+    def backup_random_projections(self) -> Tuple[torch.Tensor, List]:
+        return self.now_time.clone(), [
+            self.random_projections[i].clone() for i in range(1, self.num_layer + 1)
+        ]
+
+    def reload_random_projections(self, random_projections: Tuple) -> None:
+        f"""Reload the random projections
+
+        Args:
+            random_projections (Tuple): tuple of (now_time,random_projections)
+
+        Returns:
+        """
+        assert (
+            len(random_projections) == 2
+        ), (
+            'Expect a tuple of (now_time,random_projections)'
+        )  # @TODO: Need to raise custom exception
+        now_time, random_projections = random_projections
+        assert (
+            torch.is_tensor(now_time) and len(random_projections) == self.num_layer
+        ), (
+            'Not a valid state of random projection'
+        )  # @TODO: Need to raise custom exception
+
+        self.now_time.data = now_time.clone()
+        for i in range(1, self.num_layer + 1):
+            assert torch.is_tensor(
+                random_projections[i - 1]
+            ), (
+                'Not a valid state of random projection'
+            )  # @TODO: Need to raise custom exception
+            self.random_projections[i].data = random_projections[i - 1].clone()
 
 
 class FeedForwardNet(nn.Module):
@@ -149,7 +334,7 @@ class TPNet(nn.Module):
         Args:
             X (PyTorch Float Tensor): Node features.
             edge_index (PyTorch Tensor): Graph edge indices.
-            edge_feat (PyTorch Tensor): Edge feature vector.
+            edge_time (PyTorch Tensor): Edge time vector.
             neighbours (PyTorch Tensor): Neighbours of src and dst nodes from edge_index
             neighbours_time (PyTorch Tensor): Interaction time of src/dst nodes and their neighbours
             neighbours_edge_feat (PyTorch Tensor): Features of edge between src/dst nodes and their neighbours
@@ -161,3 +346,17 @@ class TPNet(nn.Module):
         """
         # @TODO
         raise Exception('Not yet implement')
+
+    def update_random_projection(
+        self,
+        edge_index: torch.Tensor,
+        edge_time: torch.Tensor,
+    ) -> None:
+        f"""Update the random projections of temporal walk matrices after observing positive links
+
+        Args:
+            edge_index (PyTorch Tensor): Graph edge indices.
+            edge_time (PyTorch Tensor): Edge time vector.
+        """
+        # @TODO: This need to call update from RandomProjectionModule
+        raise Exception('Not yet implemented')
