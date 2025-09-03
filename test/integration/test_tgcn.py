54c990f5795217cffcaff0f6f2a7d9cb76b2960c
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-09-03 16:42:10 -0400

    update nodeprop

--- test/integration/test_tgcn.py
+++ test/integration/test_tgcn.py
@@ -2,13 +2,13 @@ import pytest
 
 
 @pytest.mark.integration
-@pytest.mark.parametrize('dataset', ['tgbn-genre'])
+@pytest.mark.parametrize('dataset', ['tgbn-trade'])
 @pytest.mark.slurm(
     resources=[
         '--partition=main',
         '--cpus-per-task=2',
         '--mem=4G',
-        '--time=3:00:00',
+        '--time=0:10:00',
         '--gres=gpu:a100l:1',
     ]
 )
@@ -17,7 +17,7 @@ def test_tgcn_nodeprop_pred(slurm_job_runner, dataset):
 python "$ROOT_DIR/examples/nodeproppred/tgcn.py" \
     --dataset {dataset} \
     --device cuda \
-    --epochs 1
+    --epochs 50
     """
     state = slurm_job_runner(cmd)
     assert state == 'COMPLETED'
