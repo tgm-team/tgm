d12b1baa056587b482044aa7a7c9bfc7f91139fb
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-09-08 12:10:02 -0400

    Suppress only certain prints

--- tgm/data.py
+++ tgm/data.py
@@ -660,12 +660,32 @@ class DGData:
             raise ImportError('TGB required to load TGB data, try `pip install py-tgb`')
 
         def suppress_output(func: Callable, *args: Any, **kwargs: Any) -> Any:
-            import os
-            from contextlib import redirect_stdout
+            # This is a hacky workaound that tries to lower the verbosity on TGB
+            # logs which are currently directed to stdout. This should be removed
+            # once https://github.com/shenyangHuang/TGB/issues/117 is addressed.
+            import builtins
 
-            with open(os.devnull, 'w') as f:
-                with redirect_stdout(f):
-                    return func(*args, **kwargs)
+            SILENCE_PREFIXES = [
+                'raw file found',
+                'Dataset directory is',
+                'loading processed file',
+            ]
+
+            original_print = builtins.print
+
+            def filtered_print(*p_args: Any, **p_kwargs: Any) -> None:
+                if not p_args:
+                    return
+                msg = str(p_args[0])
+                if any(msg.startswith(prefix) for prefix in SILENCE_PREFIXES):
+                    return
+                original_print(*p_args, **p_kwargs)
+
+            try:
+                builtins.print = filtered_print
+                return func(*args, **kwargs)
+            finally:
+                builtins.print = original_print
 
         if name.startswith('tgbl-'):
             dataset = suppress_output(LinkPropPredDataset, name=name, **kwargs)
