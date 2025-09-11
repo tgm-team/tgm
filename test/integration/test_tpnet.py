75c159d0400a02d0272fe66d08210f1d7382110a
Bao Ngo <96505477+benjaminnNgo@users.noreply.github.com>
2025-09-11 07:38:24 -0500

    151 - TPNet implementation (#180)
    * init commit for tpnet implementation
    
    * Added skeleton of modules need to be implemented
    
    * Implemented FeedForwardNet and MLPMixer
    
    * Added draft implementation of class RandomProjectionModule for TPNet
    
    * fixed ruff failed
    
    * Draft TPNet implementation
    
    * Draft TPNet implementation
    
    * commit in-progress sample to pull update from main
    
    * TPNet
    
    Made working non-TGB example for TPNet and fixed dimension mismatch in TPNet
    
    * Fixed device issue
    
    * Added testing TGB TPNet example
    
    * TGB/tpnet.py
    
    * Move over
    
    * Update
    
    * fixed bug in rp_module.update in foward function of TPNet_LinkPrediction
    
    * Finalized TPNet implementation
    
    * Reformat code to fix ruff format fail
    
    * Refactor codes according to reviews and finalized TPNet implemntation
    
    * Refactor codes according to reviews and finalized TPNet implemntation
    
    ---------
    
    Co-authored-by: Bảo Ngô <ngot1@myumanitoba.ca>
    Co-authored-by: Bao Ngo <ngot1@gallium-01.cs.umanitoba.ca>
    Co-authored-by: Jacob-Chmura <jacobpaul.chmura@gmail.com>

---
+++ test/integration/test_tpnet.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('dataset', ['tgbl-wiki'])
+@pytest.mark.slurm(
+    resources=[
+        '--partition=main',
+        '--cpus-per-task=2',
+        '--mem=8G',
+        '--time=3:00:00',
+        '--gres=gpu:a100l:1',
+    ]
+)
+def test_tpnet_linkprop_pred(slurm_job_runner, dataset):
+    cmd = f"""
+python "$ROOT_DIR/examples/linkproppred/tpnet.py" \
+    --dataset {dataset} \
+    --device cuda \
+    --epochs 1 \
+    """
+    state = slurm_job_runner(cmd)
+    assert state == 'COMPLETED'
