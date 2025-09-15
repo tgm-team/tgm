5e97932d0055eb23ecb685280714d1a68424e7ae
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-09-15 18:48:14 -0400

    Update

--- examples/analytics/dos.py
+++ examples/analytics/dos.py
@@ -2,7 +2,8 @@ import argparse
 
 import networkx as nx
 import numpy as np
-import scipy.sparse as ss
+import scipy.sparse as sp
+from tqdm import tqdm
 
 from tgm import DGBatch, DGData, DGraph
 from tgm.hooks import HookManager, StatelessHook
@@ -23,12 +24,14 @@ parser.add_argument(
 )
 
 
-class DosHook(StatelessHook):
+class DensityOfStateEstimatorHook(StatelessHook):
     """Computes Density of States (DOS): https://arxiv.org/abs/2305.08750."""
 
     produces = {'dos'}
 
-    def __init__(self, moments: int, probing_vectors: int, n_pts: int) -> None:
+    def __init__(
+        self, moments: int = 20, probing_vectors: int = 100, n_pts: int = 50
+    ) -> None:
         self._moments = moments
         self._probing_vectors = probing_vectors
         self._n_pts = n_pts
@@ -37,7 +40,7 @@ class DosHook(StatelessHook):
     def rescale_matrix(
         A: np.ndarray, lo: int = 0, hi: int = 1, fudge: int = 0
     ) -> np.ndarray:
-        I = ss.eye(A.shape[0]) if ss.issparse(A) else np.eye(A.shape[0])
+        I = sp.eye(A.shape[0])
         a, b = (hi - lo) / (2 - fudge), (lo + hi) / 2
         return (A - b * I) / a
 
@@ -92,7 +95,7 @@ args = parser.parse_args()
 seed_everything(args.seed)
 
 dg = DGraph(DGData.from_tgb(args.dataset))
-dos_hook = DosHook(
+dos_hook = DensityOfStateEstimatorHook(
     moments=args.moments, probing_vectors=args.probing_vectors, n_pts=args.n_pts
 )
 hm = HookManager(keys=['dos'])
@@ -100,5 +103,8 @@ hm.register('dos', dos_hook)
 hm.set_active_hooks('dos')
 
 loader = DGDataLoader(dg, args.bsize, hook_manager=hm)
-for batch in loader:
-    print(batch.dos.shape)
+dos_list = []
+for batch in tqdm(loader):
+    dos_list.append(batch.dos)
+
+print(f'Computed {len(dos_list)} density state estimates')
