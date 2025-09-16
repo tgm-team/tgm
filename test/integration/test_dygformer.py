e81b18cc095d90d6d0a16c6f2da7e2e2af8e9b8b
Bao Ngo <96505477+benjaminnNgo@users.noreply.github.com>
2025-09-16 12:53:51 -0500

    Nodeproppred example for DyGFormer (#228)
    * Added draft nodeproppred for DyGFormer
    
    * Made nodeproppred for DyGFormer work
    
    * added draft for DyGFormer nodeproppred
    
    * Addressed comments
    
    ---------
    
    Co-authored-by: Bảo Ngô <ngot1@myumanitoba.ca>
    Co-authored-by: Bao Ngo <ngot1@gallium-01.cs.umanitoba.ca>

--- test/integration/test_dygformer.py
+++ test/integration/test_dygformer.py
@@ -21,3 +21,25 @@ python "$ROOT_DIR/examples/linkproppred/dygformer.py" \
     """
     state = slurm_job_runner(cmd)
     assert state == 'COMPLETED'
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize('dataset', ['tgbn-trade'])
+@pytest.mark.slurm(
+    resources=[
+        '--partition=main',
+        '--cpus-per-task=2',
+        '--mem=4G',
+        '--time=0:10:00',
+        '--gres=gpu:a100l:1',
+    ]
+)
+def test_dygformer_nodeprop_pred(slurm_job_runner, dataset):
+    cmd = f"""
+python "$ROOT_DIR/examples/nodeproppred/dygformer.py" \
+    --dataset {dataset} \
+    --device cuda \
+    --epochs 1
+    """
+    state = slurm_job_runner(cmd)
+    assert state == 'COMPLETED'
