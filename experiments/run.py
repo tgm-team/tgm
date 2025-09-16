b03d931cb030eb52ebacda8e33713dabd8dbad00
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-09-15 22:18:35 -0400

    add logging

--- experiments/run.py
+++ experiments/run.py
@@ -1,9 +1,11 @@
-from pathlib import Path
+import logging
 
 from experiments.util import (
+    EXAMPLES_ROOT,
     is_experiment_already_done,
     read_experiment_configs,
     run_experiment_as_subprocess,
+    setup_basic_logging,
 )
 
 configs = read_experiment_configs()
@@ -11,12 +13,12 @@ global_configs = configs['global_configs']
 dataset_configs = configs['datasets']
 experiment_configs = configs['experiments']
 
-examples_root = Path(__file__).resolve().parents[1] / 'examples'
+setup_basic_logging(global_configs['log_file'])
 
 
 def run_experiment(script, script_args, task, dataset, method, seed):
     if is_experiment_already_done(task, dataset, method, seed):
-        print(f'Experiment for {script} {dataset} {seed} already done, skipping')
+        logging.info(f'Experiment for {script} {dataset} {seed} already done, skipping')
     else:
         script_args.append(f'--seed {seed}')
         run_experiment_as_subprocess(script, script_args)
@@ -27,7 +29,7 @@ for task in ['linkproppred', 'nodeproppred']:
     for dataset in dataset_configs[task]:
         for experiment in experiment_configs[task]:
             method = experiment['method']
-            script = examples_root / task / f'{method}.py'
+            script = EXAMPLES_ROOT / task / f'{method}.py'
             script_args = experiment['script_flags'].split('\n')
 
             if dataset in experiment['additional_data_specific_script_flags']:
--- experiments/util.py
+++ experiments/util.py
@@ -1,10 +1,46 @@
+import argparse
 import json
+import logging
 import subprocess
 from datetime import datetime
 from pathlib import Path
+from typing import List
 
 import psutil
 import yaml
+
+EXAMPLES_ROOT = Path(__file__).resolve().parents[1] / 'examples'
+
+
+def setup_basic_logging(
+    log_file_path: str | Path | None = None,
+    log_file_logging_level: int = logging.DEBUG,
+    stream_logging_level: int = logging.INFO,
+) -> None:
+    handlers: List[logging.Handler] = []
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(stream_logging_level)
+    stream_handler.setFormatter(
+        logging.Formatter('[%(asctime)s] %(name)s - %(levelname)s %(message)s')
+    )
+    handlers.append(stream_handler)
+
+    if log_file_path is not None:
+        file_handler = logging.FileHandler(filename=log_file_path, mode='a')
+        file_handler.setLevel(log_file_logging_level)
+        file_handler.setFormatter(
+            logging.Formatter(
+                '[%(asctime)s] %(name)s - %(levelname)s [%(processName)s %(threadName)s %(name)s.%(funcName)s:%(lineno)d] %(message)s',
+            )
+        )
+        handlers.append(file_handler)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='[%(asctime)s] %(name)s - %(levelname)s [%(processName)s %(threadName)s %(name)s.%(funcName)s:%(lineno)d] %(message)s',
+        handlers=handlers,
+    )
 
 
 def read_experiment_configs():
@@ -27,9 +63,8 @@ def save_experiment_results_and_exit(results):
 
 def run_experiment_as_subprocess(script, script_args):
     cmd = f'python {script} {" ".join(script_args)}'
-    print('Running ', cmd)
+    logging.info(f'Running {cmd}')
     subprocess.run(cmd, shell=True)
-    print('Done.')
 
 
 def is_experiment_already_done(task, dataset, method, seed):
@@ -38,47 +73,48 @@ def is_experiment_already_done(task, dataset, method, seed):
     return save_path.exists()
 
 
-def setup_experiment(args, path):
+def setup_experiment(args: argparse.Namespace, path: Path) -> dict:
     results = vars(args)
     results['task'] = path.parent.stem
     results['method'] = path.stem
     results['capture_gpu'] = bool(args.capture_gpu)
     results['current_time'] = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-
-    def _get_git_hash():
-        return (
-            subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-            .decode('ascii')
-            .strip()
-        )
-
     results['git_hash'] = _get_git_hash()
-
-    def _get_cpu_info():
-        model = None
-        with open('/proc/cpuinfo') as f:
-            for line in f:
-                if line.startswith('model name'):
-                    model = line.split(':', 1)[1].strip()
-                    break  # first occurrence is enough
-        cores = sum(1 for line in open('/proc/cpuinfo') if line.startswith('processor'))
-        return f'{cores}-core {model}'
-
-    def _get_gpu_info():
-        try:
-            nvidia_smi = subprocess.check_output(
-                [
-                    'nvidia-smi',
-                    '--query-gpu=name,memory.total',
-                    '--format=csv,noheader',
-                ],
-                encoding='utf-8',
-            )
-            return ':'.join(nvidia_smi.split(', '))
-        except Exception:
-            return None
-
-    results['ram_available_gb'] = psutil.virtual_memory().total / 1024**3
+    results['ram_available_gb'] = _get_ram_available_gb()
     results['cpu_info'] = _get_cpu_info()
     results['gpu_info'] = _get_gpu_info()
     return results
+
+
+def _get_git_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+
+def _get_ram_available_gb() -> float:
+    return psutil.virtual_memory().total / 1024**3
+
+
+def _get_cpu_info() -> str | None:
+    model = None
+    with open('/proc/cpuinfo') as f:
+        for line in f:
+            if line.startswith('model name'):
+                model = line.split(':', 1)[1].strip()
+                break  # first occurrence is enough
+    cores = sum(1 for line in open('/proc/cpuinfo') if line.startswith('processor'))
+    return f'{cores}-core {model}'
+
+
+def _get_gpu_info() -> str | None:
+    try:
+        nvidia_smi = subprocess.check_output(
+            [
+                'nvidia-smi',
+                '--query-gpu=name,memory.total',
+                '--format=csv,noheader',
+            ],
+            encoding='utf-8',
+        )
+        return ':'.join(nvidia_smi.split(', '))
+    except Exception:
+        return None
