2ca093f0d479c2f22247656a32ce2799bdcc8db3
Bảo Ngô <ngot1@myumanitoba.ca>
2026-01-02 22:02:14 -0600

    Updated unit test for time

--- test/unit/test_core/test_timedelta.py
+++ test/unit/test_core/test_timedelta.py
@@ -192,9 +192,9 @@ def test_tgb_native_time_deltas():
         'thgl-github': TimeDeltaDG('s'),
         'thgl-myket': TimeDeltaDG('s'),
         'tkgl-smallpedia': TimeDeltaDG('Y'),
-        'tkgl-polecat': TimeDeltaDG('s'),
-        'tkgl-icews': TimeDeltaDG('s'),
-        'tkgl-wikidata': TimeDeltaDG('s'),
+        'tkgl-polecat': TimeDeltaDG('D'),
+        'tkgl-icews': TimeDeltaDG('D'),
+        'tkgl-wikidata': TimeDeltaDG('Y'),
     }
     assert TGB_TIME_DELTAS == exp_dict
 
