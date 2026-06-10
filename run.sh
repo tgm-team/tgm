345ea518e0b767f97bc004a6d13adbd3df213a8b
Andy Huang <andyh@Andys-MacBook-Air.local>
2026-06-09 21:49:08 -0400

    update

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=24:00:00    #48:00:00                   # The job will run for 1 day
 
 module load python/3.10
-source my_venv/bin/activate
+source .venv/bin/activate
 export UV_CACHE_DIR="/home/mila/h/huangshe/scratch/.cache"
 
 CUDA_VISIBLE_DEVICES=0 python -u examples/nodeproppred/tgn.py 
